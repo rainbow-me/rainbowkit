b489b57e3519954ade7a3130027995ced6cd1509
v1rtl <pilll.pl22@gmail.com>
2021-08-04 12:15:30 +0300

    chore: add more rollup plugins

---
+++ packages/core/src/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const useWallet = () => `Hello World`
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import ts from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       dir: 'dist',
       format: 'esm'
     }
   ],
-  plugins: [ts({ include: ['./src/**/*.ts'] })],
+  plugins: [ts({ include: ['./src/**/*.ts'] }), terser(), filesize()],
   external: ['react']
 }
