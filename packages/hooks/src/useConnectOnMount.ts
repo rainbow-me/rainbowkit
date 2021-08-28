767294ba064dce09b9bfbbe6bb382256abf548e8
v1rtl <pilll.pl22@gmail.com>
2021-08-28 17:31:36 +0300

    chore: pull from main

--- packages/hooks/src/useConnectOnMount.ts
+++ packages/hooks/src/useConnectOnMount.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { isAuthorized } from '@rainbowkit/utils'
 
@@ -6,12 +6,13 @@ import { isAuthorized } from '@rainbowkit/utils'
  * A React hook that attempts to connect to a provider if it was initialized before.
  * @param connector web3-react connector
  * @param enabled enable/disable the hook
- * @param storage browser storage to use. default is `localStorage`
+ * @param storage browser storage to use.
  */
-export const useConnectOnMount = (connector: any, enabled?: boolean, storage: Storage = localStorage) => {
+export const useConnectOnMount = (connector: any, enabled: boolean, storageProvider?: Storage | false) => {
   const { active, activate } = useWeb3React()
 
   useEffect(() => {
+    const storage = storageProvider === undefined ? localStorage : storageProvider
     let cachedState = true
     if (storage) cachedState = storage.getItem('rk-connect-on-mount') === 'true'
 
@@ -22,5 +23,5 @@ export const useConnectOnMount = (connector: any, enabled?: boolean, storage: St
     })
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [storage]) // Only trigger on mount
+  }, [storageProvider]) // Only trigger on mount
 }
