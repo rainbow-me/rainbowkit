a125027ccec53245c53e9372c4f0fc76d42379fe
v1rtl <pilll.pl22@gmail.com>
2021-08-31 14:39:19 +0300

    docs: add getting started (+ set proper peer deps)

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -3,7 +3,15 @@
 const withNextra = require('nextra')({
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  themeConfig: './theme.config.js',
+  redirects: async () => {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/get-started',
+        permanent: true
+      }
+    ]
+  }
 })
 module.exports = withNextra()
