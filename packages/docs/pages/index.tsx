7abb59f3bee0a2c41d0993b87cf9c955f76fcb08
v1rtl <pilll.pl22@gmail.com>
2021-09-07 18:24:56 +0300

    docs: add placeholder index page

---
+++ packages/docs/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <main>
+      <header>
+        <h1>RainbowKit</h1>
+        <p>The ultimate Dapp framework.</p>
+      </header>
+    </main>
+  )
+}
