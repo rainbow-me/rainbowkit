5f99e8cb7d4abaf56f023891b00a843b3362b11e
v1rtl <pilll.pl22@gmail.com>
2021-10-28 22:34:01 +0300

    feat(ui/networkselect): update designs according to mockup, add `Chain` logic support

--- packages/ui/src/components/NetworkSelect.tsx
+++ packages/ui/src/components/NetworkSelect.tsx
@@ -3,11 +3,10 @@ import { Chain, chains, switchNetwork } from '@rainbow-me/kit-utils'
 import { Web3Provider } from '@ethersproject/providers'
 import { useState } from 'react'
 import { styled } from '@linaria/react'
-import { findInSubArray, hasSubArray } from '../utils'
 import { css } from '@linaria/core'
 
 export interface NetworkSelectProps {
-  chains: string[]
+  chains: (string | Chain)[]
   provider: Web3Provider
   chainId: number
   classNames?: Partial<{
@@ -28,25 +27,28 @@ type ListProps = { isExpanded: boolean; className: string }
 
 const List = styled.div<ListProps>`
   position: absolute;
-  left: 0;
-  width: 100%;
-  padding-bottom: 1rem;
+  right: 0;
+  min-width: 160px;
+  width: max-content;
   display: ${(props) => (props.isExpanded ? 'block' : 'none')};
+  background: linear-gradient(179.83deg, rgba(26, 27, 31, 0.8) 0.15%, #1a1b1f 99.85%);
+  backdrop-filter: blur(20px);
   z-index: 10;
-  border-radius: 15px;
-  top: 70px;
-  padding-top: 20px;
+  padding: 4px;
+  border-radius: 16px;
+  top: calc(10px + 24px + 8px); /* margin + height + paddings */
+  font-weight: 800;
 `
 
 const Option = styled.div`
-  padding: 0.5rem 0.8rem;
   cursor: pointer;
   display: flex;
   align-items: center;
   flex-direction: row;
-  img {
-    margin-right: 1rem;
-  }
+  border-radius: 12px;
+  background: linear-gradient(179.83deg, rgba(26, 27, 31, 0.8) 0.15%, #1a1b1f 99.85%);
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(20px);
 `
 
 export interface ChainOptionProps {
@@ -56,7 +58,19 @@ export interface ChainOptionProps {
 }
 
 const Icon = styled.img`
-  max-height: 1.25rem;
+  min-width: 24px;
+  min-height: 24px;
+`
+
+const Indicator = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  right: 14px;
+  top: calc(50% - 4px);
+  border-radius: 50%;
+
+  background: #2ccc00;
 `
 
 export const ChainOption = ({
@@ -65,29 +79,30 @@ export const ChainOption = ({
   iconClassName,
   ...props
 }: ChainOptionProps & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => (
-  <Option aria-label="option" {...props} className={props.className}>
+  <Option aria-label="option" {...props} className={props.className || ''}>
     <Icon
       aria-hidden="true"
       className={` ${iconClassName}`}
       src={chain.logoURL || 'https://bafkreidyoljjm3jbmbewkxunvnn76s6cswo3d7ldhpnas54uphil23vlfu.ipfs.dweb.link/'}
     />{' '}
-    {chain.name} {children}
+    {children}
   </Option>
 )
 
-export const NetworkSelect = ({ chains: chainNames, provider, classNames = {}, chainId }: NetworkSelectProps) => {
+export const NetworkSelect = ({ chains: selectedChains, provider, classNames = {}, chainId }: NetworkSelectProps) => {
   const [isExpanded, setExpand] = useState(false)
 
   const currentChain = useMemo(() => chains.find((chain) => chain.chainId === chainId), [chainId])
 
-  const filteredChains = useMemo(
-    () =>
-      chains
-        .filter((chain) => hasSubArray(chainNames, chain.aliases))
-        // to sort the same way as given in props
-        .sort((a, b) => findInSubArray(chainNames, a.aliases) - findInSubArray(chainNames, b.aliases)),
-    [chainNames]
-  )
+  const filteredChains = useMemo(() => {
+    const tmp = []
+
+    for (const chain of selectedChains) {
+      if (typeof chain === 'string') tmp.push(chains.find((x) => x.aliases.includes(chain)))
+      else tmp.push(chain)
+    }
+    return tmp
+  }, [selectedChains])
 
   return (
     <Select
@@ -102,26 +117,45 @@ export const NetworkSelect = ({ chains: chainNames, provider, classNames = {}, c
           aria-selected={true}
           chain={currentChain}
           className={`${css`
-            z-index: 20;
+            padding: 7px;
+
+            /* Note: backdrop-filter has minimal browser support */
+            border-radius: 16px;
           `} ${classNames?.current}`}
           onClick={() => setExpand(!isExpanded)}
           iconClassName={classNames?.icon}
         />
       )}
       <List isExpanded={isExpanded} className={`${isExpanded ? '' : classNames?.hidden} ${classNames.list || ''}`}>
-        {filteredChains
-          .filter((ch) => ch.chainId !== chainId)
-          .map((ch) => {
-            return (
-              <ChainOption
-                chain={ch}
-                key={ch.name}
-                onClick={() => switchNetwork(provider, ch)}
-                className={classNames.option || ''}
-                iconClassName={classNames?.icon || ''}
-              />
-            )
-          })}
+        {filteredChains.map((ch) => {
+          const isCurrentChain = ch.chainId === currentChain?.chainId
+
+          return (
+            <ChainOption
+              chain={ch}
+              key={ch.name}
+              onClick={() => {
+                if (!isCurrentChain) switchNetwork(provider, ch)
+              }}
+              className={`${css`
+                padding: 10px;
+                img {
+                  margin-right: 6px;
+                }
+              `} ${
+                isCurrentChain
+                  ? css`
+                      position: relative;
+                      background: rgba(255, 255, 255, 0.06);
+                    `
+                  : ''
+              } ${classNames.option || ''}`}
+              iconClassName={classNames?.icon || ''}
+            >
+              {ch.name} {isCurrentChain && <Indicator />}
+            </ChainOption>
+          )
+        })}
       </List>
     </Select>
   )
