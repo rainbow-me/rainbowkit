0c2c7b98cbcb08041d88a021740ca7432823cf5a
v1rtl <pilll.pl22@gmail.com>
2021-11-03 11:01:30 +0200

    test(utils): `getWalletInfo`

---
+++ tests/utils/getWalletInfo.test.ts
@@ -0,0 +1,53 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { getWalletInfo } from '../../packages/utils/src/wallets'
+
+const t = suite('getWalletInfo')
+
+t('getWalletInfo', () => {
+  assert.equal(
+    ['metamask', 'coinbase', 'walletlink', 'frame', 'torus', 'rainbow', 'trust', 'gnosis', 'argent'].map((x) =>
+      getWalletInfo(x)
+    ),
+    [
+      {
+        name: 'MetaMask',
+        logoURI: 'https://bafkreig23o6p5exkyrbhxveqap3krzeinisknloocwc5uijq6wrtrlpl3e.ipfs.dweb.link'
+      },
+      {
+        name: 'Coinbase',
+        logoURI: 'https://bafkreihl4wmkbzm44mxu33mbuy3hcrc5g6wmi32ns3y3gfwme6w2hcbdy4.ipfs.dweb.link'
+      },
+      {
+        name: 'Coinbase',
+        logoURI: 'https://bafkreihl4wmkbzm44mxu33mbuy3hcrc5g6wmi32ns3y3gfwme6w2hcbdy4.ipfs.dweb.link'
+      },
+      {
+        name: 'Frame',
+        logoURI: 'https://bafkreihgrm4ebmo7ybe6vzzhwgdpiv6t4jrljl5t7y7n3keyq6n6susvra.ipfs.dweb.link'
+      },
+      {
+        name: 'Torus',
+        logoURI: 'https://bafkreiao4cnnidbqblkmd2xfb2akutm2bjdr5r4xnbuwumzhda3ikxyb7a.ipfs.dweb.link'
+      },
+      {
+        name: 'Rainbow',
+        logoURI: 'https://bafkreico3pudvsd4j6emdtq4pmyfaat34reoebxyei7tvlpzp5hnec24qa.ipfs.dweb.link'
+      },
+      {
+        name: 'Trust',
+        logoURI: 'https://bafkreiaa457sqcvunki6x7uydyjmniox22vclagwqda5qbskwd27to32aq.ipfs.dweb.link'
+      },
+      {
+        name: 'Gnosis',
+        logoURI: 'https://bafkreifsbu7uy4m25t5hty27x7nfrz3ot3wcvlnqwfujom7ax6qmixgciu.ipfs.dweb.link'
+      },
+      {
+        name: 'Argent',
+        logoURI: 'https://bafkreic5w3umuv7hz7drgyp6ymmpiqre4cnd36ftsutrrazgrecpvo5rgq.ipfs.dweb.link'
+      }
+    ]
+  )
+})
+
+t.run()
