87b50738570f1740b708f338ba7d68d79ec45c11
v1rtl <pilll.pl22@gmail.com>
2021-11-15 18:49:54 +0200

    feat(ui): more VE progress

--- packages/ui/src/components/UnsupportedNetwork.tsx
+++ packages/ui/src/components/UnsupportedNetwork.tsx
@@ -1,7 +1,7 @@
-import { css } from '@linaria/core'
-import { StyledModal, ModalTitle, ModalOverlay } from '@rainbow-me/kit-modal'
+import { ModalTitle, ModalOverlay } from '@rainbow-me/kit-modal'
 import { chainIdToName } from '@rainbow-me/kit-utils'
 import React from 'react'
+import { UnsupportedNetworkModalBodyClassName, UnsupportedNetworkModalTextClassName } from '../css/style.css'
 
 export interface UnsupportedNetworkProps {
   chainId?: number
@@ -13,25 +13,11 @@ export interface UnsupportedNetworkProps {
 export const UnsupportedNetwork = ({ chainId, chainName, supportedChainIds, isVisible }: UnsupportedNetworkProps) => {
   return (
     <ModalOverlay $isConnecting={isVisible}>
-      <div
-        className={css`
-          height: max-content;
-          max-height: 525px;
-          width: 390px;
-          padding: 24px;
-          position: relative;
-          background: #ffffff;
-          border-radius: 24px;
-          display: flex;
-          flex-direction: column;
-          p {
-            margin-bottom: 0;
-            line-height: 1.5;
-          }
-        `}
-      >
+      <div className={UnsupportedNetworkModalBodyClassName}>
         <ModalTitle>Unsupported Network: {chainName || chainIdToName(chainId)}.</ModalTitle>
-        <p>Please switch to one of the supported networks: {supportedChainIds.map(chainIdToName).join(', ')}</p>
+        <p className={UnsupportedNetworkModalTextClassName}>
+          Please switch to one of the supported networks: {supportedChainIds.map(chainIdToName).join(', ')}
+        </p>
       </div>
     </ModalOverlay>
   )
--- packages/ui/src/css/style.css.ts
+++ packages/ui/src/css/style.css.ts
@@ -41,3 +41,20 @@ export const TxContainerClassName = style({
   padding: '0.6rem 0.8rem',
   width: '100%'
 })
+
+export const UnsupportedNetworkModalBodyClassName = style({
+  height: 'max-content',
+  maxHeight: '525px',
+  width: '390px',
+  padding: '24px',
+  position: 'relative',
+  borderRadius: '24px',
+  display: 'flex',
+  flexDirection: 'column',
+  background: 'white'
+})
+
+export const UnsupportedNetworkModalTextClassName = style({
+  marginBottom: 0,
+  lineHeight: 1.5
+})
