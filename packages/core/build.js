fad56e1fbfd9b273170463f8f8ec8be82a2126c2
markdalgleish <mark.john.dalgleish@gmail.com>
2021-12-17 15:35:10 +1100

    chore: add dev script that starts watch build and example app

--- packages/core/build.js
+++ packages/core/build.js
@@ -2,10 +2,27 @@ import * as esbuild from 'esbuild'
 import { readdir } from 'fs/promises'
 import { addExtension, externals } from '../../esbuild/plugins.js'
 
-esbuild.build({
-  entryPoints: (await readdir('src')).map((x) => `./src/${x}`),
-  bundle: true,
-  format: 'esm',
-  plugins: [externals, addExtension],
-  outdir: 'dist'
-})
+const isWatching = process.argv.includes('--watch')
+
+esbuild
+  .build({
+    entryPoints: (await readdir('src')).map((x) => `./src/${x}`),
+    bundle: true,
+    format: 'esm',
+    plugins: [externals, addExtension],
+    watch: isWatching
+      ? {
+          onRebuild(error, result) {
+            if (error) console.error('watch build failed:', error)
+            else console.log('watch build succeeded:', result)
+          }
+        }
+      : undefined,
+    outdir: 'dist'
+  })
+  .then(() => {
+    if (isWatching) {
+      console.log('watching...')
+    }
+  })
+  .catch(() => process.exit(1))
