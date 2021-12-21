1ad9ce66e5e6c76d18ae18222d323f807c14e33e
Nick Beattie <nick@nickbytes.com>
2021-12-21 11:07:33 -0800

    fix: network switching with WalletConnect (#58)
    * fix: network switching
    
    refactor: wallets and networks with chain switching
    
    fix: close() + simpler network
    
    fix: network behavior
    
    * chore: fix types, must be any?

--- packages/utils/src/network.ts
+++ packages/utils/src/network.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { hexStripZeros } from '@ethersproject/bytes'
 import { Web3Provider } from '@ethersproject/providers'
 import type { Chain } from './chains'
 
@@ -14,28 +16,31 @@ export const switchNetwork = async (provider: Web3Provider, chain: Chain) => {
     rpcUrls: chain.rpc
   }
 
+  const formattedChainId = hexStripZeros(BigNumber.from(chain.chainId).toHexString())
+
   try {
-    if (chain.explorers) {
-      await provider.send('wallet_addEthereumChain', [
-        {
-          ...obj,
-          blockExplorerUrls: [chain.explorers[0].url]
-        }
-      ])
-    }
+    await provider.send('wallet_switchEthereumChain', [
+      {
+        chainId: formattedChainId
+      }
+    ])
+  } catch (error: any) {
+    // walletconnect-connector throws an "JSON RPC response format is invalid", but
+    // this error is not correct. The RPC response is valid, and the network has changed.
+    // Other L2-centric apps recognize this error and switch the network.
+    console.error(error)
 
-    await provider.send('wallet_addEthereumChain', [obj])
-  } catch (e) {
-    console.error(e)
-
-    try {
-      await provider.send('wallet_switchEthereumChain', [
-        {
-          chainId: `0x${chain.chainId.toString(16)}`
-        }
-      ])
-    } catch (e) {
-      console.error(e)
+    // checks if metamask error
+    if (error.code === 4902) {
+      try {
+        // metamask (only known implementer) automatically switches after a network is added
+        // the second call is done here because that behavior is not a part of the spec and cannot be relied upon in the future
+        // metamask's behavior when switching to the current network is just to return null (a no-op)
+        await provider.send('wallet_addEthereumChain', [obj])
+        await switchNetwork(provider, chain)
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
