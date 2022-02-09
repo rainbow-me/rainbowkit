0fe2a67e49dae25506859a3bc024744bfac73e91
Nick Beattie <nick@nickbytes.com>
2022-02-09 11:18:02 -0800

    fix: exhaustive deps

--- packages/rainbowkit/src/components/ConnectButton/ConnectButton.tsx
+++ packages/rainbowkit/src/components/ConnectButton/ConnectButton.tsx
@@ -25,7 +25,7 @@ export function ConnectButton() {
       connect(walletConnectDefault);
     }
     setIsMounted(true);
-  }, [setIsMounted]);
+  }, [setIsMounted, connect, walletConnectDefault, connectData.connector]);
 
   if (!isMounted) {
     return null;
