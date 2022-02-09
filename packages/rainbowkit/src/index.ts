7860e9dfaa43fc08ac07003bc3620d433e2e38ee
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-02-10 08:54:14 +1100

    fix: resolve wagmi peer dependency issue (#89)

--- packages/rainbowkit/src/index.ts
+++ packages/rainbowkit/src/index.ts
@@ -8,11 +8,3 @@ export { darkTheme } from './themes/darkTheme';
 export { dimTheme } from './themes/dimTheme';
 export { cssStringFromTheme } from './css/cssStringFromTheme';
 export { cssObjectFromTheme } from './css/cssObjectFromTheme';
-
-// We're re-exporting from wagmi for now to resolve peer dependency
-// resolution issues due to mixed usage of ESM and CJS. Otherwise
-// rainbowkit and the host app get different instances of wagmi,
-// meaning they can't talk to each other because they have different
-// React contexts. This is not intended to be the final API.
-export * from 'wagmi';
-export { Provider as WagmiProvider } from 'wagmi';
