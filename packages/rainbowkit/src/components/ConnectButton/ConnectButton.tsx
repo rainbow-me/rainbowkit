97f6a1f6c202128848af6db945fdf50a4c07ecc7
Nick Beattie <nick@nickbytes.com>
2022-02-09 21:46:44 -0800

    feat: adds useIsMounted hook for convenience

--- packages/rainbowkit/src/components/ConnectButton/ConnectButton.tsx
+++ packages/rainbowkit/src/components/ConnectButton/ConnectButton.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAccount } from 'wagmi';
+import { useIsMounted } from '../../hooks/useIsMounted';
 import { Box } from '../Box/Box';
 import { Network } from '../Network/Network';
 import { Profile } from '../Profile/Profile';
 import { Connect } from './Connect';
 
 export function ConnectButton() {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, [setIsMounted]);
+  const isMounted = useIsMounted();
 
   const [{ data: accountData }] = useAccount({
     fetchEns: true,
