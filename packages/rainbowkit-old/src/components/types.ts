49072df09dd9f5708e923d6d17af30c0a9509c01
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-02-12 09:43:40 +1100

    chore: clean up unused code, tests and dependencies (#98)

--- packages/rainbowkit-old/src/components/types.ts
+++
@@ -1,3 +0,0 @@
-import { SVGProps } from 'react';
-
-export type IconProps = SVGProps<SVGSVGElement>;
--- packages/rainbowkit-old/src/utils/wc.ts
+++
@@ -1,9 +0,0 @@
-import { chains } from './chains';
-
-const walletConnectRPCs: Record<number, string> = {};
-
-for (const chain of chains) {
-  walletConnectRPCs[chain.chainId] = chain.rpc[0];
-}
-
-export { walletConnectRPCs };
