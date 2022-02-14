11c711ac9d17cdd84d3571d302a49e2b19394c19
Pedro Duarte <oi@ped.ro>
2022-02-14 16:23:44 +0100

    feat: animate dialog on enter

--- packages/rainbowkit/src/components/Dialog/Dialog.css.ts
+++ packages/rainbowkit/src/components/Dialog/Dialog.css.ts
@@ -1,5 +1,10 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
 import { sprinkles } from '../../css/sprinkles.css';
+
+const slideUp = keyframes({
+  '0%': { transform: 'translateY(100%)' },
+  '100%': { transform: 'translateY(0deg)' },
+});
 
 export const overlay = style([
   sprinkles({
@@ -24,6 +29,7 @@ export const content = style([
     position: 'relative',
   }),
   {
+    animation: `${slideUp} 500ms cubic-bezier(0.16, 1, 0.3, 1)`,
     width: '390px',
   },
 ]);
