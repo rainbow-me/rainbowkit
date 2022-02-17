ba98c798115b1865a79a93e7b00b2802840fcc5f
Nico Glennon <nicoglennon@icloud.com>
2022-02-17 14:11:21 -0500

    fix: new spinner icon

--- packages/rainbowkit/src/components/Icons/Icons.css.ts
+++ packages/rainbowkit/src/components/Icons/Icons.css.ts
@@ -15,5 +15,11 @@ const spin = keyframes({
 });
 
 export const SpinnerIconClassName = style({
-  animation: `${spin} 4s infinite linear`,
+  animation: `${spin} 3s infinite linear`,
+});
+
+export const SpinnerIconPathClassName = style({
+  background: `conic-gradient(from 180deg at 50% 50%, rgba(72, 146, 254, 0) 0deg, #4892FE 282.04deg, rgba(72, 146, 254, 0) 319.86deg, rgba(72, 146, 254, 0) 360deg)`,
+  height: 21,
+  width: 21,
 });
