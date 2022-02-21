20864455765356d0a5bf45a30acbcc4de4a8dee7
markdalgleish <mark.john.dalgleish@gmail.com>
2022-02-22 06:20:35 +1100

    docs: update wallets API usage on docs site

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/no-danger */
 import '@rainbow-me/rainbowkit/styles.css';
-import { Chain, darkTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {
+  Chain,
+  connectorsForWallets,
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
 import { providers } from 'ethers';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { chain, Provider as WagmiProvider } from 'wagmi';
-import { InjectedConnector } from 'wagmi/connectors/injected';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { WalletLinkConnector } from 'wagmi/connectors/walletLink';
 import '../css/global.css';
 
 const infuraId = '0c8c992691dc4bfe97b4365a27fb2ce4';
@@ -22,32 +25,16 @@ const chains: Chain[] = [
   { ...chain.arbitrumOne, name: 'Arbitrum' },
 ];
 
-const connectors = ({ chainId }) => {
-  const rpcUrl =
+const wallets = getDefaultWallets({
+  appName: 'RainbowKit demo',
+  chains,
+  infuraId,
+  jsonRpcUrl: ({ chainId }) =>
     chains.find(x => x.id === chainId)?.rpcUrls?.[0] ??
-    chain.mainnet.rpcUrls[0];
+    chain.mainnet.rpcUrls[0],
+});
 
-  return [
-    new InjectedConnector({
-      chains,
-      options: { shimDisconnect: true },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        infuraId,
-        qrcode: true,
-      },
-    }),
-    new WalletLinkConnector({
-      chains,
-      options: {
-        appName: 'RainbowKit demo',
-        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
-      },
-    }),
-  ];
-};
+const connectors = connectorsForWallets(wallets);
 
 function App({ Component, pageProps }: AppProps) {
   return (
