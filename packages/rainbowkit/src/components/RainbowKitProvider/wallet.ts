cd8e15cdb70435fceb7cec3795fbb7d77d610645
markdalgleish <mark.john.dalgleish@gmail.com>
2022-02-28 14:22:13 +1100

    fix: use WalletConnect fallback for MetaMask on mobile

--- packages/rainbowkit/src/components/RainbowKitProvider/wallet.ts
+++ packages/rainbowkit/src/components/RainbowKitProvider/wallet.ts
@@ -3,6 +3,7 @@ import { Connector, useConnect, Chain as WagmiChain } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { WalletLinkConnector } from 'wagmi/connectors/walletLink';
+import { isMobile } from '../../utils/isMobile';
 import { Chain } from './ChainIconsContext';
 
 export type WalletConnectorConfig<C extends Connector = Connector> = {
@@ -83,13 +84,13 @@ const rainbow = ({ chains, infuraId }: RainbowOptions): Wallet => {
             setTimeout(() => {
               const { uri } = connector.getProvider().connector;
 
-              if (uri) {
-                window.location.href = /android/i.test(navigator.userAgent)
-                  ? uri
-                  : `https://rnbwapp.com/wc?uri=${encodeURIComponent(uri)}`;
-              } else {
+              if (!uri) {
                 throw new Error('No WalletConnect URI found');
               }
+
+              window.location.href = /android/i.test(navigator.userAgent)
+                ? uri
+                : `https://rnbwapp.com/wc?uri=${encodeURIComponent(uri)}`;
             }, 0);
           }, [connect, connectData.connector]),
         };
@@ -150,20 +151,64 @@ const coinbase =
 
 interface MetamaskOptions {
   chains: Chain[];
+  infuraId: string;
   shimDisconnect?: boolean;
 }
 const metamask =
-  ({ chains, shimDisconnect }: MetamaskOptions): Wallet =>
-  () => ({
-    connector: new InjectedConnector({
-      chains,
-      options: { shimDisconnect },
-    }),
-    iconUrl:
-      'https://cloudflare-ipfs.com/ipfs/QmdaG1gGZDAhSzQuicSHD32ernCzgB8p72WvnBDTUDrRNh',
-    id: 'metamask',
-    name: 'Metamask',
-  });
+  ({ chains, infuraId, shimDisconnect }: MetamaskOptions): Wallet =>
+  () => {
+    const shouldUseWalletConnect =
+      isMobile() &&
+      typeof window !== 'undefined' &&
+      // @ts-expect-error
+      (!window.ethereum || !window.ethereum.isMetaMask);
+
+    const connector = shouldUseWalletConnect
+      ? new WalletConnectConnector({
+          chains,
+          options: {
+            infuraId,
+            qrcode: false,
+          },
+        })
+      : new InjectedConnector({
+          chains,
+          options: { shimDisconnect },
+        });
+
+    return {
+      connector,
+      iconUrl:
+        'https://cloudflare-ipfs.com/ipfs/QmdaG1gGZDAhSzQuicSHD32ernCzgB8p72WvnBDTUDrRNh',
+      id: 'metamask',
+      name: 'Metamask',
+      useMobileWalletButton: !shouldUseWalletConnect
+        ? undefined
+        : () => {
+            const [{ data: connectData }, connect] = useConnect();
+
+            return {
+              onClick: useCallback(() => {
+                if (connectData.connector !== connector) {
+                  connect(connector);
+                }
+
+                setTimeout(() => {
+                  const { uri } = connector.getProvider().connector;
+
+                  if (!uri) {
+                    throw new Error('No WalletConnect URI found');
+                  }
+
+                  window.location.href = `https://metamask.app.link/wc?uri=${encodeURIComponent(
+                    uri
+                  )}`;
+                }, 0);
+              }, [connect, connectData.connector]),
+            };
+          },
+    };
+  };
 
 export const wallet = {
   coinbase,
@@ -186,7 +231,7 @@ export const getDefaultWallets = ({
   wallet.rainbow({ chains, infuraId }),
   wallet.walletConnect({ chains, infuraId }),
   wallet.coinbase({ appName, chains, jsonRpcUrl }),
-  wallet.metamask({ chains, shimDisconnect: true }),
+  wallet.metamask({ chains, infuraId, shimDisconnect: true }),
 ];
 
 export const connectorsForWallets = (wallets: Wallet[] = []) => {
