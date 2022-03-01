e8db5ad940741283a39fb5016ee5f748583a643a
Nico Glennon <nicoglennon@icloud.com>
2022-03-01 18:38:59 -0500

    feat: stepflow scaffold

--- packages/rainbowkit/src/components/ConnectModal/ConnectModalIntro.tsx
+++ packages/rainbowkit/src/components/ConnectModal/ConnectModalIntro.tsx
@@ -5,37 +5,43 @@ import { LoginIcon } from '../Icons/Login';
 import { Text } from '../Text/Text';
 import { introTextClassName } from './ConnectModalIntro.css';
 
-export function ConnectModalIntro() {
+export function ConnectModalIntro({ getWallet }: { getWallet: () => void }) {
   return (
-    <Box
-      alignItems="center"
-      color="accentColor"
-      display="flex"
-      flexDirection="column"
-      gap="16"
-      justifyContent="center"
-    >
-      <LoginIcon />
-      <Text color="modalText" size="18" weight="bold">
-        A New Way to Log In
-      </Text>
-      <Text className={introTextClassName} color="modalTextSecondary" size="16">
-        Instead of a creating a new username and password on every site, you can
-        use a wallet to log in instantly
-      </Text>
+    <>
       <Box
         alignItems="center"
+        color="accentColor"
         display="flex"
         flexDirection="column"
         gap="16"
         justifyContent="center"
-        marginTop="16"
       >
-        <Button label="Get a Wallet" onClick={() => {}} />
-        <Text color="accentColor" weight="bold">
-          Learn More
+        <LoginIcon />
+        <Text color="modalText" size="18" weight="bold">
+          A New Way to Log In
         </Text>
+        <Text
+          className={introTextClassName}
+          color="modalTextSecondary"
+          size="16"
+        >
+          Instead of a creating a new username and password on every site, you
+          can use a wallet to log in instantly
+        </Text>
+        <Box
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+          gap="16"
+          justifyContent="center"
+          marginTop="16"
+        >
+          <Button label="Get a Wallet" onClick={getWallet} />
+          <Text color="accentColor" weight="bold">
+            Learn More
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
