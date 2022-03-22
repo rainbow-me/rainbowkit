0724d88ef0afb52f8b604843a4d3f37650d21d75
Nico Glennon <nicoglennon@icloud.com>
2022-03-22 12:51:28 -0400

    fix: mobile separators & spacing in txs

--- packages/rainbowkit/src/components/Txs/TxItem.tsx
+++ packages/rainbowkit/src/components/Txs/TxItem.tsx
@@ -7,6 +7,7 @@ import {
   TransactionWithInfo,
 } from '../../hooks/useTxHistory';
 import { chainIdToExplorerLink } from '../../utils/chainIdToExplorerLink';
+import { isMobile } from '../../utils/isMobile';
 import { Box } from '../Box/Box';
 import { CancelIcon } from '../Icons/Cancel';
 import { ExternalLinkIcon } from '../Icons/ExternalLink';
@@ -33,6 +34,7 @@ interface TxProps {
 }
 
 export function TxItem({ tx }: TxProps) {
+  const mobile = isMobile();
   const Icon = getTxStatusIcon(tx.status);
   const color = tx.status === FAIL_TX_STATUS ? 'error' : 'accentColor';
   const [{ data: networkData }] = useNetwork();
@@ -47,54 +49,99 @@ export function TxItem({ tx }: TxProps) {
   const explorerLink = chainIdToExplorerLink(networkData?.chain?.id);
 
   return (
-    <Box
-      {...(explorerLink
-        ? {
-            as: 'a',
-            background: { hover: 'profileForeground' },
-            borderRadius: 'menuButton',
-            href: `${explorerLink}/tx/${tx.hash}`,
-            rel: 'noreferrer',
-            target: '_blank',
-            transform: { active: 'shrink', hover: 'grow' },
-            transition: 'default',
-          }
-        : {})}
-      color="modalText"
-      display="flex"
-      flexDirection="row"
-      justifyContent="space-between"
-      padding="8"
-    >
-      <Box alignItems="center" display="flex" flexDirection="row" gap="14">
-        <Box color={color}>
-          <Icon />
-        </Box>
-        <Box display="flex" flexDirection="column" gap="2">
-          <Box>
-            <Text color="modalText" font="body" size="14" weight="bold">
-              {tx?.info}
-            </Text>
+    // <<<<<<< HEAD
+    // <Box
+    //   {...(explorerLink
+    //     ? {
+    //         as: 'a',
+    //         background: { hover: 'profileForeground' },
+    //         borderRadius: 'menuButton',
+    //         href: `${explorerLink}/tx/${tx.hash}`,
+    //         rel: 'noreferrer',
+    //         target: '_blank',
+    //         transform: { active: 'shrink', hover: 'grow' },
+    //         transition: 'default',
+    //       }
+    //     : {})}
+    //   color="modalText"
+    //   display="flex"
+    //   flexDirection="row"
+    //   justifyContent="space-between"
+    //   padding="8"
+    // >
+    //   <Box alignItems="center" display="flex" flexDirection="row" gap="14">
+    //     <Box color={color}>
+    //       <Icon />
+    //     </Box>
+    //     <Box display="flex" flexDirection="column" gap="2">
+    //       <Box>
+    //         <Text color="modalText" font="body" size="14" weight="bold">
+    //           {tx?.info}
+    //         </Text>
+    // =======
+    <>
+      <Box
+        {...(explorerLink
+          ? {
+              as: 'a',
+              background: { hover: 'profileForeground' },
+              borderRadius: 'menuButton',
+              href: `${explorerLink}/tx/${tx.hash}`,
+              rel: 'noreferrer',
+              target: '_blank',
+              transform: { active: 'shrink', hover: 'grow' },
+              transition: 'default',
+            }
+          : {})}
+        color="modalText"
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        padding="8"
+      >
+        <Box alignItems="center" display="flex" flexDirection="row" gap="14">
+          <Box color={color}>
+            <Icon />
           </Box>
-          <Box>
-            <Text
-              color={
-                tx.status === PENDING_TX_STATUS ? 'modalTextSecondary' : color
-              }
-              font="body"
-              size="14"
-              weight="regular"
-            >
-              {confirmationStatus}
-            </Text>
+          <Box display="flex" flexDirection="column" gap="2">
+            <Box>
+              <Text
+                color="modalText"
+                font="body"
+                size={mobile ? '16' : '14'}
+                weight="bold"
+              >
+                {tx?.info}
+              </Text>
+            </Box>
+            <Box>
+              <Text
+                color={
+                  tx.status === PENDING_TX_STATUS ? 'modalTextSecondary' : color
+                }
+                font="body"
+                size="14"
+                weight="regular"
+              >
+                {confirmationStatus}
+              </Text>
+            </Box>
           </Box>
         </Box>
+        {explorerLink && (
+          <Box alignItems="center" color="modalTextDim" display="flex">
+            <ExternalLinkIcon />
+          </Box>
+        )}
       </Box>
-      {explorerLink && (
-        <Box alignItems="center" color="modalTextDim" display="flex">
-          <ExternalLinkIcon />
-        </Box>
+      {mobile && (
+        <Box
+          background="generalBorder"
+          height="1"
+          marginLeft="44"
+          marginRight="8"
+        />
       )}
-    </Box>
+    </>
   );
 }
