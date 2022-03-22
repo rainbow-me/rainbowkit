6df9c502daa51caf78ea7ab6d3d7101ef760cc65
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-03-22 12:21:14 +1100

    fix: handle missing explorer URLs (#181)

--- packages/rainbowkit/src/utils/chainIdToExplorerLink.ts
+++ packages/rainbowkit/src/utils/chainIdToExplorerLink.ts
@@ -1,8 +1,7 @@
 import { ChainId, chainInfo } from './chains';
 
-export const chainIdToExplorerLink = (chainId?: ChainId): string => {
-  if (!chainId) {
-    return `${chainInfo[ChainId.MAINNET].explorer}/address/`;
-  }
-  return `${chainInfo[chainId].explorer}/address/`;
+export const chainIdToExplorerLink = (chainId?: ChainId): string | null => {
+  return chainId !== undefined && chainInfo[chainId]
+    ? `${chainInfo[chainId].explorer.replace(/\/$/, '')}`
+    : null;
 };
