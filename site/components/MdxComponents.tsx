e1cee66a85dc0d2391044140771064c4c4db3215
Mike Arndt <hello@mikearndt.net>
2022-03-29 23:26:56 -0700

    fix: update styles (#207)
    - Smaller info icons for table
    - Heavier code font weight
    - Shadow and transition effect to left rail menu
    - Layouts changes on: li, h2, h3, etc
    - New li style

--- site/components/MdxComponents.tsx
+++ site/components/MdxComponents.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 /* eslint-disable jsx-a11y/heading-has-content */
 import { pre } from 'css/pre.css';
-import { body, headline, text, title2, titleLarge } from 'css/text.css';
+import { body, text, title2, title3, titleLarge } from 'css/text.css';
 import { vars } from 'css/vars.css';
 import React from 'react';
 import { Link } from './Link/Link';
@@ -11,16 +11,27 @@ export const components = {
   h1: props => <h1 className={titleLarge} {...props} />,
   h2: props => (
     <h2
-      className={headline}
+      className={title3}
       {...props}
-      style={{ color: vars.colors.labelSecondary, marginBottom: 24 }}
+      style={{
+        color: vars.colors.labelSecondary,
+        marginTop: vars.space[2],
+        marginBottom: vars.space[6],
+        fontWeight: '500',
+      }}
     />
   ),
-  h3: props => <h3 className={title2} {...props} />,
+  h3: props => (
+    <h3 className={title2} {...props} style={{ marginTop: vars.space[11] }} />
+  ),
   p: props => (
     <p
       className={body}
-      style={{ fontWeight: 600, color: vars.colors.labelSecondary }}
+      style={{
+        fontWeight: 500,
+        color: vars.colors.labelSecondary,
+        lineHeight: '25px',
+      }}
       {...props}
     />
   ),
@@ -29,10 +40,13 @@ export const components = {
     // eslint-disable-next-line jsx-a11y/alt-text
     <img {...props} style={{ display: 'block', maxWidth: '100%' }} />
   ),
-  ul: props => <ul style={{ paddingLeft: vars.space[4] }} {...props} />,
   ol: props => <ol style={{ paddingLeft: vars.space[4] }} {...props} />,
   li: props => (
-    <li className={text[3]} style={{ lineHeight: 1.5 }} {...props} />
+    <li
+      className={text[3]}
+      style={{ lineHeight: 1.5, display: 'flex', alignItems: 'center' }}
+      {...props}
+    />
   ),
   pre: ({ children, ...props }) => {
     return (
