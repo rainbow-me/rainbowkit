5f0335d41b929fec315ea2eb7af11953aa8e31a4
markdalgleish <mark.john.dalgleish@gmail.com>
2022-04-13 12:27:43 +1000

    fix: lint

--- packages/rainbowkit/src/components/Icons/Spinner.tsx
+++ packages/rainbowkit/src/components/Icons/Spinner.tsx
@@ -21,9 +21,9 @@ const useClipPathId = (enabled: boolean) => {
 };
 
 export const SpinnerIcon = ({
+  forceRegularRenderInSafari = false,
   height = 21,
   width = 21,
-  forceRegularRenderInSafari = false,
 }: {
   width?: string | number;
   height?: string | number;
@@ -36,10 +36,10 @@ export const SpinnerIcon = ({
       className={SpinnerIconClassName}
       fill="none"
       height={height}
+      id={id}
       viewBox="0 0 21 21"
       width={width}
       xmlns="http://www.w3.org/2000/svg"
-      id={id}
     >
       <clipPath id={id}>
         <path d="M10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18C11.3284 18 12 18.6716 12 19.5C12 20.3284 11.3284 21 10.5 21C4.70101 21 0 16.299 0 10.5C0 4.70101 4.70101 0 10.5 0C16.299 0 21 4.70101 21 10.5C21 11.3284 20.3284 12 19.5 12C18.6716 12 18 11.3284 18 10.5C18 6.35786 14.6421 3 10.5 3Z" />
