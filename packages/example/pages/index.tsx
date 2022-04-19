d0d22d062ad00ecaf14f764037dee281ca876857
Nick Beattie <nick@nickbytes.com>
2022-04-19 12:24:57 -0700

    refactor: inlines data

--- packages/example/pages/index.tsx
+++ packages/example/pages/index.tsx
@@ -14,39 +14,6 @@ type ExtractString<Value> = Value extends string ? Value : never;
 type AccountStatus = ExtractString<ConnectButtonProps['accountStatus']>;
 type ChainStatus = ExtractString<ConnectButtonProps['chainStatus']>;
 
-// All properties on a domain are optional
-const domain = {
-  chainId: 1,
-  name: 'Ether Mail',
-  verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-  version: '1',
-};
-
-// The named list of all type definitions
-const types = {
-  Mail: [
-    { name: 'from', type: 'Person' },
-    { name: 'to', type: 'Person' },
-    { name: 'contents', type: 'string' },
-  ],
-  Person: [
-    { name: 'name', type: 'string' },
-    { name: 'wallet', type: 'address' },
-  ],
-};
-
-const value = {
-  contents: 'Hello, Bob!',
-  from: {
-    name: 'Cow',
-    wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-  },
-  to: {
-    name: 'Bob',
-    wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-  },
-};
-
 const Example = () => {
   const [{ data: accountData }] = useAccount();
   const defaultProps = ConnectButton.__defaultProps;
@@ -85,9 +52,34 @@ const Example = () => {
 
   const [{ data: typedData, error: typedError }, signTypedData] =
     useSignTypedData({
-      domain,
-      types,
-      value,
+      domain: {
+        chainId: 1,
+        name: 'Ether Mail',
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+        version: '1',
+      },
+      types: {
+        Mail: [
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person' },
+          { name: 'contents', type: 'string' },
+        ],
+        Person: [
+          { name: 'name', type: 'string' },
+          { name: 'wallet', type: 'address' },
+        ],
+      },
+      value: {
+        contents: 'Hello, Bob!',
+        from: {
+          name: 'Cow',
+          wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        },
+        to: {
+          name: 'Bob',
+          wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        },
+      },
     });
 
   return (
