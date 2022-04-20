0c592bb894f37e85bec3798df5912b48fa457a2e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-04-20 21:30:15 +1000

    chore: update to React v18, remove next-transpiled-modules (#260)

--- site/components/DocsMobileMenu/DocsMobileMenu.tsx
+++ site/components/DocsMobileMenu/DocsMobileMenu.tsx
@@ -1,14 +1,20 @@
 import { Portal } from '@radix-ui/react-portal';
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 export const DocsMobileMenuContext = React.createContext<
   React.RefObject<HTMLDivElement>
 >({} as any);
 
+function useMounted() {
+  const [mounted, mount] = useReducer(() => true, false);
+  useEffect(mount, [mount]);
+  return mounted;
+}
+
 export function DocsMobileMenuSlot({ children }) {
   const docsMobileMenuRef = React.useContext(DocsMobileMenuContext);
 
-  return typeof document !== 'undefined' ? (
+  return useMounted() ? (
     <Portal containerRef={docsMobileMenuRef}>{children}</Portal>
   ) : null;
 }
--- site/components/Provider/Provider.tsx
+++ site/components/Provider/Provider.tsx
@@ -1,4 +1,3 @@
-import '@rainbow-me/rainbowkit/styles.css';
 import {
   Chain,
   connectorsForWallets,
