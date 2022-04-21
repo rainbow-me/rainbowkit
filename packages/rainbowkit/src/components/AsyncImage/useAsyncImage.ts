51b584e30571c0209db7d2f560969b402db6188b
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-04-21 12:03:35 +1000

    fix: handle async image errors (#259)

--- packages/rainbowkit/src/components/AsyncImage/useAsyncImage.ts
+++ packages/rainbowkit/src/components/AsyncImage/useAsyncImage.ts
@@ -5,28 +5,43 @@ export type AsyncImageSrc = () => Promise<string>;
 const cachedUrls = new Map<AsyncImageSrc, string>();
 
 // Store requests in a cache so we don't fetch the same image twice
-const cachedRequestPromises = new Map<AsyncImageSrc, Promise<string>>();
+const cachedRequestPromises = new Map<AsyncImageSrc, Promise<string | void>>();
 
-async function loadAsyncImage(loadImageUrl: () => Promise<string>) {
-  const cachedRequestPromise = cachedRequestPromises.get(loadImageUrl);
+async function loadAsyncImage(asyncImage: () => Promise<string>) {
+  const cachedRequestPromise = cachedRequestPromises.get(asyncImage);
 
   // Don't fetch if we already have a request in progress / completed
   if (cachedRequestPromise) {
     return cachedRequestPromise;
   }
 
-  const requestPromise = loadImageUrl().then(async url => {
-    // Uncomment to simulate slow image loading:
-    // await new Promise(resolve =>
-    //   setTimeout(resolve, 2000 + Math.random() * 1000)
-    // );
+  const load = async () =>
+    asyncImage().then(async (url: string) => {
+      // Uncomment to simulate slow image loading:
+      // await new Promise(resolve =>
+      //   setTimeout(resolve, 2000 + Math.random() * 1000)
+      // );
 
-    cachedUrls.set(loadImageUrl, url);
+      // Uncomment to simulate random failure:
+      // if (Math.random() > 0.25) {
+      //   throw new Error();
+      // }
 
-    return url;
+      cachedUrls.set(asyncImage, url);
+
+      return url;
+    });
+
+  const requestPromise = load().catch(_err => {
+    // Retry once if the request failed
+    return load().catch(_err => {
+      // Ignore failed retry, remove failed request from
+      // promise cache so next request can try again
+      cachedRequestPromises.delete(asyncImage);
+    });
   });
 
-  cachedRequestPromises.set(loadImageUrl, requestPromise);
+  cachedRequestPromises.set(asyncImage, requestPromise);
 
   return requestPromise;
 }
