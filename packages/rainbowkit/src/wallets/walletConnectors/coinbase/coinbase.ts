ce2a1223e0631c6d75028349526acde7e9be7159
Nick Beattie <nick@nickbytes.com>
2022-04-21 16:57:06 -0700

    feat: use correct connector

--- packages/rainbowkit/src/wallets/walletConnectors/coinbase/coinbase.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/coinbase/coinbase.ts
@@ -1,7 +1,8 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
+import { InjectedConnector } from 'wagmi';
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
-import { isIOS } from '../../../utils/isMobile';
+import { isIOS, isMobile } from '../../../utils/isMobile';
 import { Wallet } from '../../Wallet';
 
 export interface CoinbaseOptions {
@@ -14,43 +15,55 @@ export const coinbase = ({
   appName,
   chains,
   jsonRpcUrl,
-}: CoinbaseOptions): Wallet => ({
-  id: 'coinbase',
-  name: 'Coinbase Wallet',
-  shortName: 'Coinbase',
-  iconUrl: async () => (await import('./coinbase.svg')).default,
-  iconBackground: '#2c5ff6',
-  downloadUrls: {
-    browserExtension:
-      'https://chrome.google.com/webstore/detail/coinbase-wallet-extension/hnfanknocfeofbddgcijnmhnfnkdnaad',
-    android: 'https://play.google.com/store/apps/details?id=org.toshi',
-    ios: 'https://apps.apple.com/us/app/coinbase-wallet-store-crypto/id1278383455',
-  },
-  createConnector: ({ chainId }) => {
-    const ios = isIOS();
+}: CoinbaseOptions): Wallet => {
+  const isCoinbaseInjected =
+    typeof window !== 'undefined' &&
+    // @ts-expect-error
+    window.ethereum?.isCoinbaseWallet;
 
-    const connector = new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName,
-        headlessMode: true,
-        jsonRpcUrl:
-          typeof jsonRpcUrl === 'function'
-            ? jsonRpcUrl({ chainId })
-            : jsonRpcUrl,
-      },
-    });
+  const shouldUseQRCode = isMobile() && !isCoinbaseInjected;
 
-    const getUri = () => connector.getProvider().qrUrl;
+  return {
+    id: 'coinbase',
+    name: 'Coinbase Wallet',
+    shortName: 'Coinbase',
+    iconUrl: async () => (await import('./coinbase.svg')).default,
+    iconBackground: '#2c5ff6',
+    installed: !shouldUseQRCode ? isCoinbaseInjected : undefined,
+    downloadUrls: {
+      browserExtension:
+        'https://chrome.google.com/webstore/detail/coinbase-wallet-extension/hnfanknocfeofbddgcijnmhnfnkdnaad',
+      android: 'https://play.google.com/store/apps/details?id=org.toshi',
+      ios: 'https://apps.apple.com/us/app/coinbase-wallet-store-crypto/id1278383455',
+    },
+    createConnector: ({ chainId }) => {
+      const ios = isIOS();
 
-    return {
-      connector: connector,
-      ...(ios
-        ? {}
-        : {
-            mobile: { getUri },
-            qrCode: { getUri },
-          }),
-    };
-  },
-});
+      const connector = shouldUseQRCode
+        ? new CoinbaseWalletConnector({
+            chains,
+            options: {
+              appName,
+              headlessMode: true,
+              jsonRpcUrl:
+                typeof jsonRpcUrl === 'function'
+                  ? jsonRpcUrl({ chainId })
+                  : jsonRpcUrl,
+            },
+          })
+        : new InjectedConnector({ chains });
+
+      const getUri = () => connector.getProvider().qrUrl;
+
+      return {
+        connector,
+        ...(ios
+          ? {}
+          : {
+              mobile: { getUri },
+              qrCode: { getUri },
+            }),
+      };
+    },
+  };
+};
