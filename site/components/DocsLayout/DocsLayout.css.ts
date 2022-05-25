8fc33e7af8ded4d6eb55bd9e64e2ae0ba9994cc0
Pedro Duarte <oi@ped.ro>
2022-05-25 18:49:00 +0200

    fix: clipped shadow (#416)

--- site/components/DocsLayout/DocsLayout.css.ts
+++ site/components/DocsLayout/DocsLayout.css.ts
@@ -1,8 +1,9 @@
 import { style } from '@vanilla-extract/css';
+import { breakpoints } from 'css/breakpoints';
 import { responsiveStyle } from 'css/responsiveStyle';
 import { vars } from 'css/vars.css';
 
-const SIDEBAR_SIZE = 250;
+const SIDEBAR_SIZE = 264;
 
 export const navigationSidebar = style([
   {
@@ -14,7 +15,7 @@ export const navigationSidebar = style([
     lg: {
       bottom: 0,
       display: 'block',
-      left: 'calc(50% - 512px)',
+      left: `calc(50% - ${breakpoints.lg / 2}px)`,
       paddingRight: 0,
       paddingTop: vars.space[11],
       position: 'fixed',
@@ -28,6 +29,7 @@ export const navigationSidebarScroller = style([
   {
     height: '100%',
     overflow: 'auto',
+    padding: '0 7px',
   },
 ]);
 
@@ -38,7 +40,7 @@ export const content = style([
   },
   responsiveStyle({
     lg: {
-      paddingLeft: SIDEBAR_SIZE,
+      paddingLeft: SIDEBAR_SIZE - 14,
     },
   }),
 ]);
