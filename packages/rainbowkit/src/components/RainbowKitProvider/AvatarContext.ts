dfc7d13858a242600c1d0ed89fe7594b3e8e95dd
Nico Glennon <nicoglennon@icloud.com>
2022-06-02 19:25:20 -0400

    feat: custom avatar (#451)
    * feat: custom avatar
    
    * fix: use default EmojiAvatar comp in context
    
    * fix: streamline custom avatar component
    
    * chore: add changeset and documentation
    
    * chore: update to make avatar plural
    
    * chore: update to make avatar plural
    
    * chore: update borderRadius
    
    * chore: formatting mdx
    
    Co-authored-by: Nick Beattie <nick@nickbytes.com>

---
+++ packages/rainbowkit/src/components/RainbowKitProvider/AvatarContext.ts
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react';
+import { EmojiAvatar } from '../Avatar/EmojiAvatar';
+
+export type AvatarComponentProps = {
+  address: string;
+  ensImage?: string | null;
+  size: number;
+};
+export type AvatarComponent = React.FunctionComponent<AvatarComponentProps>;
+
+export const defaultAvatar = EmojiAvatar;
+
+export const AvatarContext = createContext<AvatarComponent>(defaultAvatar);
