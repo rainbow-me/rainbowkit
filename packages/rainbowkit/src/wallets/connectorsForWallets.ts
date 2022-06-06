244b4b54935337f5c86cadb13569aa770e82c3f2
moxey.eth <jakemoxey@gmail.com>
2022-06-06 10:54:35 +1000

    refactor: remove deprecated usage of wagmi connectors

--- packages/rainbowkit/src/wallets/connectorsForWallets.ts
+++ packages/rainbowkit/src/wallets/connectorsForWallets.ts
@@ -1,61 +1,59 @@
 import { Connector } from 'wagmi';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { omitUndefinedValues } from '../utils/omitUndefinedValues';
-import { ConnectorArgs, WalletInstance, WalletList } from './Wallet';
+import { WalletInstance, WalletList } from './Wallet';
 
 export const connectorsForWallets = (walletList: WalletList) => {
-  return function (connectorArgs: ConnectorArgs) {
-    const connectors: Connector[] = [];
+  const connectors: Connector[] = [];
 
-    walletList.forEach(({ groupName, wallets }) => {
-      wallets.forEach(({ createConnector, ...walletMeta }) => {
-        const { connector, ...connectionMethods } = omitUndefinedValues(
-          createConnector(connectorArgs)
+  walletList.forEach(({ groupName, wallets }) => {
+    wallets.forEach(({ createConnector, ...walletMeta }) => {
+      const { connector, ...connectionMethods } = omitUndefinedValues(
+        createConnector()
+      );
+
+      // @ts-expect-error
+      if (connector._wallet) {
+        throw new Error(
+          `Can't connect wallet "${walletMeta.name}" to connector "${
+            connector.name ?? connector.id
+          }" as it's already connected to wallet "${
+            // @ts-expect-error
+            connector._wallet.name
+          }". Each wallet must have its own connector instance.`
         );
+      }
 
-        // @ts-expect-error
-        if (connector._wallet) {
-          throw new Error(
-            `Can't connect wallet "${walletMeta.name}" to connector "${
-              connector.name ?? connector.id
-            }" as it's already connected to wallet "${
-              // @ts-expect-error
-              connector._wallet.name
-            }". Each wallet must have its own connector instance.`
-          );
-        }
+      let walletConnectModalConnector: Connector | undefined;
+      if (walletMeta.id === 'walletConnect' && connectionMethods.qrCode) {
+        const { chains, options } = connector;
 
-        let walletConnectModalConnector: Connector | undefined;
-        if (walletMeta.id === 'walletConnect' && connectionMethods.qrCode) {
-          const { chains, options } = connector;
+        walletConnectModalConnector = new WalletConnectConnector({
+          chains,
+          options: {
+            ...options,
+            qrcode: true,
+          },
+        });
 
-          walletConnectModalConnector = new WalletConnectConnector({
-            chains,
-            options: {
-              ...options,
-              qrcode: true,
-            },
-          });
+        connectors.push(walletConnectModalConnector);
+      }
 
-          connectors.push(walletConnectModalConnector);
-        }
+      const walletInstance: WalletInstance = {
+        connector,
+        groupName,
+        walletConnectModalConnector,
+        ...walletMeta,
+        ...connectionMethods,
+      };
 
-        const walletInstance: WalletInstance = {
-          connector,
-          groupName,
-          walletConnectModalConnector,
-          ...walletMeta,
-          ...connectionMethods,
-        };
+      // Mutate connector instance to add wallet instance
+      // @ts-expect-error
+      connector._wallet = walletInstance;
 
-        // Mutate connector instance to add wallet instance
-        // @ts-expect-error
-        connector._wallet = walletInstance;
-
-        connectors.push(connector);
-      });
+      connectors.push(connector);
     });
+  });
 
-    return connectors;
-  };
+  return connectors;
 };
