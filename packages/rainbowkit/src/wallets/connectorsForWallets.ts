81e7333805bbcec4f6ef3968f25e976d90d5f0a4
markdalgleish <mark.john.dalgleish@gmail.com>
2022-06-15 11:47:08 +1000

    fix: reinstate lazy connectors to avoid duplicate wallets

--- packages/rainbowkit/src/wallets/connectorsForWallets.ts
+++ packages/rainbowkit/src/wallets/connectorsForWallets.ts
@@ -5,57 +5,59 @@ import { omitUndefinedValues } from '../utils/omitUndefinedValues';
 import { WalletInstance, WalletList } from './Wallet';
 
 export const connectorsForWallets = (walletList: WalletList) => {
-  let index = -1;
+  return () => {
+    let index = -1;
 
-  const connectors: Connector[] = [];
+    const connectors: Connector[] = [];
 
-  walletList.forEach(({ groupName, wallets }) => {
-    wallets.forEach(({ createConnector, ...walletMeta }) => {
-      index++;
+    walletList.forEach(({ groupName, wallets }) => {
+      wallets.forEach(({ createConnector, ...walletMeta }) => {
+        index++;
 
-      const { connector, ...connectionMethods } = omitUndefinedValues(
-        createConnector()
-      );
+        const { connector, ...connectionMethods } = omitUndefinedValues(
+          createConnector()
+        );
 
-      let walletConnectModalConnector: Connector | undefined;
-      if (
-        walletMeta.id === 'walletConnect' &&
-        connectionMethods.qrCode &&
-        !isMobile()
-      ) {
-        const { chains, options } = connector;
+        let walletConnectModalConnector: Connector | undefined;
+        if (
+          walletMeta.id === 'walletConnect' &&
+          connectionMethods.qrCode &&
+          !isMobile()
+        ) {
+          const { chains, options } = connector;
 
-        walletConnectModalConnector = new WalletConnectConnector({
-          chains,
-          options: {
-            ...options,
-            qrcode: true,
-          },
-        });
+          walletConnectModalConnector = new WalletConnectConnector({
+            chains,
+            options: {
+              ...options,
+              qrcode: true,
+            },
+          });
 
-        connectors.push(walletConnectModalConnector);
-      }
+          connectors.push(walletConnectModalConnector);
+        }
 
-      const walletInstance: WalletInstance = {
-        connector,
-        groupName,
-        index,
-        walletConnectModalConnector,
-        ...walletMeta,
-        ...connectionMethods,
-      };
+        const walletInstance: WalletInstance = {
+          connector,
+          groupName,
+          index,
+          walletConnectModalConnector,
+          ...walletMeta,
+          ...connectionMethods,
+        };
 
-      // Mutate connector instance to add wallet instance
-      // @ts-expect-error
-      connector._wallets = connector._wallets ?? [];
-      // @ts-expect-error
-      connector._wallets.push(walletInstance);
+        // Mutate connector instance to add wallet instance
+        // @ts-expect-error
+        connector._wallets = connector._wallets ?? [];
+        // @ts-expect-error
+        connector._wallets.push(walletInstance);
 
-      if (!connectors.includes(connector)) {
-        connectors.push(connector);
-      }
+        if (!connectors.includes(connector)) {
+          connectors.push(connector);
+        }
+      });
     });
-  });
 
-  return connectors;
+    return connectors;
+  };
 };
