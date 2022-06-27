d905271ad797563c32988ec01deb00a6ceb13478
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-06-28 07:53:52 +1000

    fix: ensure blank tab isn't left behind after connecting on iOS (#540)

--- packages/rainbowkit/src/components/ConnectOptions/MobileOptions.tsx
+++ packages/rainbowkit/src/components/ConnectOptions/MobileOptions.tsx
@@ -58,7 +58,20 @@ function WalletButton({ wallet }: { wallet: WalletConnector }) {
             setWalletConnectDeepLink({ mobileUri, name });
 
             if (mobileUri.startsWith('http')) {
-              window.open(mobileUri, '_blank', 'noreferrer,noopener');
+              // Workaround for https://github.com/rainbow-me/rainbowkit/issues/524.
+              // Using 'window.open' causes issues on iOS in non-Safari browsers and
+              // WebViews where a blank tab is left behind after connecting.
+              // This is especially bad in some WebView scenarios (e.g. following a
+              // link from Twitter) where the user doesn't have any mechanism for
+              // closing the blank tab.
+              // For whatever reason, links with a target of "_blank" don't suffer
+              // from this problem, and programmatically clicking a detached link
+              // element with the same attributes also avoids the issue.
+              const link = document.createElement('a');
+              link.href = mobileUri;
+              link.target = '_blank';
+              link.rel = 'noreferrer noopener';
+              link.click();
             } else {
               window.location.href = mobileUri;
             }
