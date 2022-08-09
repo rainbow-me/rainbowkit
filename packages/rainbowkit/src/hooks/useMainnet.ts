4df45827cf480e651fa41e8cf965193221132d06
markdalgleish <mark.john.dalgleish@gmail.com>
2022-08-09 14:38:45 +1000

    fix: support filtering provider chains

--- packages/rainbowkit/src/hooks/useMainnet.ts
+++ packages/rainbowkit/src/hooks/useMainnet.ts
@@ -1,13 +1,17 @@
-import { chain } from 'wagmi';
-import { useRainbowKitChains } from '../components/RainbowKitProvider/RainbowKitChainContext';
+import { Chain, useProvider, chain as wagmiChains } from 'wagmi';
 
 export function useMainnet() {
-  const rainbowKitChains = useRainbowKitChains();
+  const chainId = wagmiChains.mainnet.id;
 
-  const chainId = chain.mainnet.id;
-  const enabled = rainbowKitChains.some(
-    rainbowKitChain => rainbowKitChain.id === chainId
-  );
+  // Because the generic for 'useProvider' is defaulting to 'unknown'
+  // and the return type is being resolved as 'any', we're having to
+  // manually define the Provider type, so this code is more defensive
+  // than necessary in case the manual typing is ever incorrect.
+  // If we're unable to resolve a list of chains, or the chains are
+  // an invalid type, we'll silently bail out.
+  const provider = useProvider<{ chains?: Chain[] }>();
+  const chains = Array.isArray(provider.chains) ? provider.chains : [];
+  const enabled = chains?.some(chain => chain?.id === chainId);
 
   return { chainId, enabled };
 }
