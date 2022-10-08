ed4bb0398a2a6f74a48aca7e5b4a2ab7fb31af66
chendatony31 <31647753@qq.com>
2022-10-08 20:24:27 +0800

    fix: optimize tokenpocket wallet

--- packages/rainbowkit/src/wallets/walletConnectors/tokenPocketWallet/tokenPocketWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/tokenPocketWallet/tokenPocketWallet.ts
@@ -1,7 +1,7 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
-import { isMobile } from '../../../utils/isMobile';
+import { isAndroid } from '../../../utils/isMobile';
 import { Wallet } from '../../Wallet';
 import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 
@@ -17,7 +17,7 @@ export const tokenPocketWallet = ({
   const isTokenPocketInjected =
     typeof window !== 'undefined' && window.ethereum?.isTokenPocket === true;
 
-  const shouldUseWalletConnect = isMobile() && !isTokenPocketInjected;
+  const shouldUseWalletConnect = !isTokenPocketInjected;
 
   return {
     id: 'tokenPocket',
@@ -30,7 +30,8 @@ export const tokenPocketWallet = ({
         'https://chrome.google.com/webstore/detail/tokenpocket/mfgccjchihfkkindfppnaooecgfneiii',
       android:
         'https://play.google.com/store/apps/details?id=vip.mytokenpocket',
-      ios: 'https://apps.apple.com/cn/app/tokenpocket-trusted-wallet/id1436028697',
+      ios: 'https://apps.apple.com/us/app/tokenpocket-trusted-wallet/id1436028697',
+      qrCode: 'https://download.tokenpocket.pro/',
     },
     createConnector: () => {
       const connector = shouldUseWalletConnect
@@ -43,9 +44,64 @@ export const tokenPocketWallet = ({
           getUri: shouldUseWalletConnect
             ? async () => {
                 const { uri } = (await connector.getProvider()).connector;
-                return `tpoutside://wc?uri=${encodeURIComponent(uri)}`;
+                return isAndroid()
+                  ? uri
+                  : `tpoutside://wc?uri=${encodeURIComponent(uri)}`;
               }
             : undefined,
+        },
+        qrCode: shouldUseWalletConnect
+          ? {
+              getUri: async () => (await connector.getProvider()).connector.uri,
+              instructions: {
+                learnMoreUrl: 'https://help.tokenpocket.pro/en/',
+                steps: [
+                  {
+                    description:
+                      'We recommend putting TokenPocket on your home screen for quicker access.',
+                    step: 'install',
+                    title: 'Open the TokenPocket app',
+                  },
+                  {
+                    description:
+                      'Be sure to back up your wallet using a secure method. Never share your secret phrase with anyone.',
+                    step: 'create',
+                    title: 'Create or Import a Wallet',
+                  },
+                  {
+                    description:
+                      'After you scan, a connection prompt will appear for you to connect your wallet.',
+                    step: 'scan',
+                    title: 'Tap the scan button',
+                  },
+                ],
+              },
+            }
+          : undefined,
+        extension: {
+          learnMoreUrl: 'https://help.tokenpocket.pro/en/',
+          instructions: {
+            steps: [
+              {
+                description:
+                  'We recommend pinning TokenPocket to your taskbar for quicker access to your wallet.',
+                step: 'install',
+                title: 'Install the TokenPocket extension',
+              },
+              {
+                description:
+                  'Be sure to back up your wallet using a secure method. Never share your secret phrase with anyone.',
+                step: 'create',
+                title: 'Create or Import a Wallet',
+              },
+              {
+                description:
+                  'Once you set up your wallet, click below to refresh the browser and load up the extension.',
+                step: 'refresh',
+                title: 'Refresh your browser',
+              },
+            ],
+          },
         },
       };
     },
