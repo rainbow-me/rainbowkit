bfaa90f0e96dcd1c398f31494c5071c954e08818
andrewdahn <andrewahnjr@gmail.com>
2022-10-13 21:15:30 -0700

    docs: define QR prop

--- packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
@@ -1,9 +1,9 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
+import { isAndroid } from '../../../utils/isMobile';
 import { Wallet } from '../../Wallet';
 import { getWalletConnectConnector } from '../../getWalletConnectConnector';
-
 export interface MewWalletOptions {
   chains: Chain[];
   shimDisconnect?: boolean;
@@ -45,14 +45,37 @@ export const mewWallet = ({
 
     return {
       connector,
-      desktop: {
-        getUri: async () => {
-          return 'https://download.mewwallet.com?source=rainbow';
-        },
-      },
       mobile: {
         getUri: async () => {
-          return 'https://download.mewwallet.com?source=rainbow';
+          const { uri } = (await connector.getProvider()).connector;
+          return isAndroid()
+            ? uri
+            : `https://mewwallet.com/wc?uri=${encodeURIComponent(uri)}`;
+        },
+      },
+      qrCode: {
+        getUri: async () => (await connector.getProvider()).connector.uri,
+        instructions: {
+          learnMoreUrl: 'https://www.mewwallet.com/what-is-ethereum/',
+          steps: [
+            {
+              description:
+                'Put MEW wallet on your home screen for faster access.',
+              step: 'install',
+              title: 'Open the MEW wallet app',
+            },
+            {
+              description: 'Create a new wallet or import an existing one.',
+              step: 'create',
+              title: 'Create or Import a Wallet',
+            },
+            {
+              description:
+                'Tap the camera icon, scan the QR code,  and confirm the prompt to connect.',
+              step: 'scan',
+              title: 'Tap the QR scan icon in Home',
+            },
+          ],
         },
       },
     };
