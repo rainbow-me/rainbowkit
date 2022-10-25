68eab1ad514f786345e75b4cb3dc1d550c6f622b
Nico Glennon <nicoglennon@icloud.com>
2022-10-25 18:01:53 -0400

    feat: have rainbow connector check `isRainbow`

--- packages/rainbowkit/src/wallets/walletConnectors/rainbowWallet/rainbowWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/rainbowWallet/rainbowWallet.ts
@@ -1,4 +1,5 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
+import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
 import { isAndroid } from '../../../utils/isMobile';
 import { Wallet } from '../../Wallet';
@@ -6,59 +7,117 @@ import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 
 export interface RainbowWalletOptions {
   chains: Chain[];
+  shimDisconnect?: boolean;
 }
 
-export const rainbowWallet = ({ chains }: RainbowWalletOptions): Wallet => ({
-  id: 'rainbow',
-  name: 'Rainbow',
-  iconUrl: async () => (await import('./rainbowWallet.svg')).default,
-  iconBackground: '#0c2f78',
-  downloadUrls: {
-    android: 'https://play.google.com/store/apps/details?id=me.rainbow',
-    ios: 'https://apps.apple.com/us/app/rainbow-ethereum-wallet/id1457119021',
-    qrCode: 'https://rainbow.download',
-  },
-  createConnector: () => {
-    const connector = getWalletConnectConnector({ chains });
+function isRainbow(ethereum: NonNullable<typeof window['ethereum']>) {
+  // isRainbow needs to be added to the wagmi `Ethereum` object
+  // @ts-ignore
+  const isRainbow = Boolean(ethereum.isRainbow);
 
-    return {
-      connector,
-      mobile: {
-        getUri: async () => {
-          const { uri } = (await connector.getProvider()).connector;
+  if (!isRainbow) {
+    return false;
+  }
 
-          return isAndroid()
-            ? uri
-            : `https://rnbwapp.com/wc?uri=${encodeURIComponent(uri)}`;
+  return true;
+}
+
+export const rainbowWallet = ({
+  chains,
+  shimDisconnect,
+}: RainbowWalletOptions): Wallet => {
+  const isRainbowInjected =
+    typeof window !== 'undefined' &&
+    typeof window.ethereum !== 'undefined' &&
+    isRainbow(window.ethereum);
+
+  const shouldUseWalletConnect = !isRainbowInjected;
+  return {
+    id: 'rainbow',
+    name: 'Rainbow',
+    iconUrl: async () => (await import('./rainbowWallet.svg')).default,
+    iconBackground: '#0c2f78',
+    downloadUrls: {
+      browserExtension: 'tbd',
+      android: 'https://play.google.com/store/apps/details?id=me.rainbow',
+      ios: 'https://apps.apple.com/us/app/rainbow-ethereum-wallet/id1457119021',
+      qrCode: 'https://rainbow.download',
+    },
+    createConnector: () => {
+      const connector = shouldUseWalletConnect
+        ? getWalletConnectConnector({ chains })
+        : new InjectedConnector({
+            chains,
+            options: { shimDisconnect },
+          });
+
+      const getUri = async () => {
+        const { uri } = (await connector.getProvider()).connector;
+
+        return isAndroid()
+          ? uri
+          : `https://rnbwapp.com/wc?uri=${encodeURIComponent(uri)}`;
+      };
+      // const connector = getWalletConnectConnector({ chains });
+
+      return {
+        connector,
+        mobile: { getUri: shouldUseWalletConnect ? getUri : undefined },
+        qrCode: shouldUseWalletConnect
+          ? {
+              getUri,
+              instructions: {
+                learnMoreUrl:
+                  'https://learn.rainbow.me/connect-your-wallet-to-a-website-or-app',
+                steps: [
+                  {
+                    description:
+                      'We recommend putting Rainbow on your home screen for faster access to your wallet.',
+                    step: 'install',
+                    title: 'Open the Rainbow app',
+                  },
+                  {
+                    description:
+                      'You can easily backup your wallet using our backup feature on your phone.',
+                    step: 'create',
+                    title: 'Create or Import a Wallet',
+                  },
+                  {
+                    description:
+                      'After you scan, a connection prompt will appear for you to connect your wallet.',
+                    step: 'scan',
+                    title: 'Tap the scan button',
+                  },
+                ],
+              },
+            }
+          : undefined,
+        extension: {
+          learnMoreUrl: 'https://rainbow.me/learn',
+          instructions: {
+            steps: [
+              {
+                description:
+                  'We recommend pinning Rainbow to your taskbar for quicker access to your wallet.',
+                step: 'install',
+                title: 'Install the Rainbow extension',
+              },
+              {
+                description:
+                  'Be sure to back up your wallet using a secure method. Never share your secret phrase with anyone.',
+                step: 'create',
+                title: 'Create or Import a Wallet',
+              },
+              {
+                description:
+                  'Once you set up your wallet, click below to refresh the browser and load up the extension.',
+                step: 'refresh',
+                title: 'Refresh your browser',
+              },
+            ],
+          },
         },
-      },
-      qrCode: {
-        getUri: async () => (await connector.getProvider()).connector.uri,
-        instructions: {
-          learnMoreUrl:
-            'https://learn.rainbow.me/connect-your-wallet-to-a-website-or-app',
-          steps: [
-            {
-              description:
-                'We recommend putting Rainbow on your home screen for faster access to your wallet.',
-              step: 'install',
-              title: 'Open the Rainbow app',
-            },
-            {
-              description:
-                'You can easily backup your wallet using our backup feature on your phone.',
-              step: 'create',
-              title: 'Create or Import a Wallet',
-            },
-            {
-              description:
-                'After you scan, a connection prompt will appear for you to connect your wallet.',
-              step: 'scan',
-              title: 'Tap the scan button',
-            },
-          ],
-        },
-      },
-    };
-  },
-});
+      };
+    },
+  };
+};
