5ed61cc30e38ca98d3b1adb593be8310b781f3fd
Daniel Sinclair <d@niel.nyc>
2023-01-16 17:13:10 -0500

    fix: updated iron-session example for new SiweResponse type

--- examples/with-next-siwe-iron-session/pages/api/verify.ts
+++ examples/with-next-siwe-iron-session/pages/api/verify.ts
@@ -10,12 +10,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       try {
         const { message, signature } = req.body;
         const siweMessage = new SiweMessage(message);
-        const fields = await siweMessage.verify({ signature });
+        const { success, error, data } = await siweMessage.verify({
+          signature,
+        });
 
-        if (fields.nonce !== req.session.nonce)
+        if (!success) throw error;
+
+        if (data.nonce !== req.session.nonce)
           return res.status(422).json({ message: 'Invalid nonce.' });
 
-        req.session.siwe = fields;
+        req.session.siwe = data;
         await req.session.save();
         res.json({ ok: true });
       } catch (_error) {
