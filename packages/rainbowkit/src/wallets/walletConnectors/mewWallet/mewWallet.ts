27c629b51fb05a27e2cdfc52a0010335ce8311fa
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-02-01 12:04:10 -0800

    fix: id, qr code

--- packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
@@ -13,14 +13,15 @@ export const mewWallet = ({
   chains,
   shimDisconnect,
 }: MewWalletOptions): Wallet => ({
-  id: 'mewWallet',
-  name: 'MEW wallet',
+  id: 'mew',
+  name: 'MEW Wallet',
   iconUrl: async () => (await import('./mewWallet.svg')).default,
   iconBackground: '#fff',
   downloadUrls: {
     android:
       'https://play.google.com/store/apps/details?id=com.myetherwallet.mewwallet&referrer=utm_source%3Drainbow',
     ios: 'https://apps.apple.com/app/apple-store/id1464614025?pt=118781877&mt=8&ct=rainbow',
+    qrCode: 'https://www.mewwallet.com/',
   },
   createConnector: () => {
     const isMewWalletInjected =
