fb6fa4c3cac57f359825213205c084c49c5d4dbe
Daniel Sinclair <d@niel.nyc>
2023-02-03 16:50:13 -0500

    fix: resolved typescript error

--- packages/rainbowkit/src/utils/omitUndefinedValues.ts
+++ packages/rainbowkit/src/utils/omitUndefinedValues.ts
@@ -1,5 +1,6 @@
 export function omitUndefinedValues<T>(obj: T): T {
   return Object.fromEntries(
+    // @ts-expect-error
     Object.entries(obj).filter(([_key, value]) => value !== undefined)
   ) as T;
 }
