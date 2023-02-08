f0a36b514055fe1a745407aa8ca4251ec5962afa
Stas SUȘCOV <stas@nerd.ro>
2023-02-08 18:41:17 +0000

    Added Safe (Gnosis) connector.

---
+++ packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
@@ -0,0 +1,24 @@
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+import { SafeConnector, SafeConnectorOptions } from 'wagmi/connectors/safe';
+import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
+import { Wallet } from '../../Wallet';
+
+export interface SafeWalletOptions {
+  chains: Chain[];
+  options?: SafeConnectorOptions;
+}
+
+// See the official integration guide for auto-connecting
+// https://github.com/safe-global/safe-apps-sdk/tree/main/packages/safe-apps-wagmi#integration-steps
+export const safeWallet = ({
+  chains,
+  options = {}
+}: SafeWalletOptions): Wallet => ({
+  id: 'safe',
+  name: 'Safe (Gnosis)',
+  iconUrl: async () => (await import('./safe-logo.svg')).default,
+  iconBackground: '#000',
+  createConnector: () => ({
+    connector: new SafeConnector({ chains, options }),
+  }),
+});
