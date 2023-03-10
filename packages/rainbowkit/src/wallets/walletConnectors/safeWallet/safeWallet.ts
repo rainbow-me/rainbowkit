3db153b4431d61f41d91056d6e94c03be4775be4
Daniel Sinclair <d@niel.nyc>
2023-03-10 14:51:40 -0500

    fix: cleaned up safe connector options and installed logic

--- packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
@@ -1,24 +1,33 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-import { SafeConnector, SafeConnectorOptions } from 'wagmi/connectors/safe';
+import { SafeConnector } from 'wagmi/connectors/safe';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
 import { Wallet } from '../../Wallet';
 
 export interface SafeWalletOptions {
   chains: Chain[];
-  options?: SafeConnectorOptions;
+  shimDisconnect?: boolean;
 }
 
-// See the official integration guide for auto-connecting
-// https://github.com/safe-global/safe-apps-sdk/tree/main/packages/safe-apps-wagmi#integration-steps
 export const safeWallet = ({
   chains,
-  options = {},
+  shimDisconnect,
 }: SafeWalletOptions): Wallet => ({
   id: 'safe',
   name: 'Safe',
+  iconAccent: '#12ff80',
+  iconBackground: '#fff',
   iconUrl: async () => (await import('./safeWallet.svg')).default,
-  iconBackground: '#000',
+  installed:
+    // Only allowed in iframe context
+    // borrowed from wagmi safe connector
+    !(typeof window === 'undefined') && window?.parent !== window,
+  downloadUrls: {
+    // We're opting not to provide a download prompt if Safe isn't the current
+    // browser since it's unlikely to be a desired behavior for users. It's
+    // more of a convenience for users who are already using Brave rather than
+    // an explicit wallet choice for users coming from other browsers.
+  },
   createConnector: () => ({
-    connector: new SafeConnector({ chains, options }),
+    connector: new SafeConnector({ chains, options: { shimDisconnect } }),
   }),
 });
