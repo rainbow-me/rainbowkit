d39a201e75696084752499271998055b8ad82ee1
Daniel Sinclair <d@niel.nyc>
2023-03-20 02:14:38 -0400

    chore: added changeset and cleaned up shimDisconnect pass-through

--- packages/rainbowkit/src/wallets/walletConnectors/tahoWallet/tahoWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/tahoWallet/tahoWallet.ts
@@ -1,10 +1,11 @@
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+import type { InjectedConnectorOptions } from '@wagmi/core/connectors/injected';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
 import { Wallet } from '../../Wallet';
 
-export interface InjectedWalletOptions {
+export interface TahoWalletOptions {
   chains: Chain[];
-  shimDisconnect?: boolean;
 }
 
 declare global {
@@ -15,37 +16,36 @@ declare global {
 
 export const tahoWallet = ({
   chains,
-  shimDisconnect,
-}: InjectedWalletOptions): Wallet => ({
+  ...options
+}: TahoWalletOptions & InjectedConnectorOptions): Wallet => ({
+  id: 'taho',
+  name: 'Taho',
+  iconBackground: '#d08d57',
+  iconUrl: async () => (await import('./tahoWallet.svg')).default,
+  downloadUrls: {
+    browserExtension:
+      'https://chrome.google.com/webstore/detail/taho/eajafomhmkipbjmfmhebemolkcicgfmd',
+  },
+  installed:
+    typeof window !== 'undefined' &&
+    typeof window.tally !== 'undefined' &&
+    window['tally']
+      ? true
+      : undefined,
   createConnector: () => {
     return {
       connector: new InjectedConnector({
         chains,
         options: {
-          getProvider() {
+          getProvider: () => {
             const getTaho = (tally?: any) =>
               tally?.isTally ? tally : undefined;
             if (typeof window === 'undefined') return;
             return getTaho(window.tally);
           },
-          name: 'Taho',
-          shimDisconnect,
+          ...options,
         },
       }),
     };
   },
-  downloadUrls: {
-    browserExtension:
-      'https://chrome.google.com/webstore/detail/taho/eajafomhmkipbjmfmhebemolkcicgfmd',
-  },
-  iconBackground: '#d08d57',
-  iconUrl: async () => (await import('./tahoWallet.svg')).default,
-  id: 'taho',
-  installed:
-    typeof window !== 'undefined' &&
-    typeof window.tally !== 'undefined' &&
-    Boolean(window['tally'])
-      ? true
-      : false,
-  name: 'Taho',
 });
