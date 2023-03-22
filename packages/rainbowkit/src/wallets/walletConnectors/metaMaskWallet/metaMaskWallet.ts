77ec63648d8246b998accb19edfe5a917ed53bf8
Daniel Sinclair <d@niel.nyc>
2023-03-22 13:57:37 -0400

    chore: updated isMetaMask detection logic

--- packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
@@ -10,29 +10,21 @@ export interface MetaMaskWalletOptions {
   chains: Chain[];
 }
 
-function isMetaMask(ethereum: NonNullable<typeof window['ethereum']>) {
+function isMetaMask(ethereum?: typeof window['ethereum']): boolean {
   // Logic borrowed from wagmi's MetaMaskConnector
-  // https://github.com/tmm/wagmi/blob/main/packages/core/src/connectors/metaMask.ts
-  const isMetaMask = Boolean(ethereum.isMetaMask);
-
-  if (!isMetaMask) {
-    return false;
-  }
-
+  // https://github.com/wagmi-dev/references/blob/main/packages/connectors/src/metaMask.ts
+  const isMetaMask = !!ethereum?.isMetaMask;
+  if (!isMetaMask) return false;
   // Brave tries to make itself look like MetaMask
   // Could also try RPC `web3_clientVersion` if following is unreliable
-  if (ethereum.isBraveWallet && !ethereum._events && !ethereum._state) {
+  if (ethereum.isBraveWallet && !ethereum._events && !ethereum._state)
     return false;
-  }
-
-  if (ethereum.isTokenPocket) {
-    return false;
-  }
-
-  if (ethereum.isTokenary) {
-    return false;
-  }
-
+  if (ethereum.isApexWallet) return false;
+  if (ethereum.isAvalanche) return false;
+  if (ethereum.isKuCoinWallet) return false;
+  if (ethereum.isPortal) return false;
+  if (ethereum.isTokenPocket) return false;
+  if (ethereum.isTokenary) return false;
   return true;
 }
 
