31ce20a687403301c41af5f5075a1181c972cc60
Daniel Sinclair <d@niel.nyc>
2023-03-31 21:29:44 -0400

    fix: amended rainbowWallet connector for extension support

--- packages/rainbowkit/src/wallets/walletConnectors/rainbowWallet/rainbowWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/rainbowWallet/rainbowWallet.ts
@@ -36,12 +36,14 @@ export const rainbowWallet = ({
     name: 'Rainbow',
     iconUrl: async () => (await import('./rainbowWallet.svg')).default,
     iconBackground: '#0c2f78',
+    installed: !shouldUseWalletConnect ? isRainbowInjected : undefined,
     downloadUrls: {
       android:
         'https://play.google.com/store/apps/details?id=me.rainbow&referrer=utm_source%3Drainbowkit',
       ios: 'https://apps.apple.com/app/apple-store/id1457119021?pt=119997837&ct=rainbowkit&mt=8',
       qrCode:
         'https://rainbow.download?utm_source=rainbowkit&utm_medium=qrcode',
+      browserExtension: 'https://rainbow.me/extension?utm_source=rainbowkit',
     },
     createConnector: () => {
       const connector = shouldUseWalletConnect
