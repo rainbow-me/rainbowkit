f314fa7f2eb968e4e44eb2c91aa5906de264acf7
Daniel Sinclair <d@niel.nyc>
2023-04-04 23:43:09 -0400

    example: updated with-next-rainbow-button

--- examples/with-next-rainbow-button/pages/index.tsx
+++ examples/with-next-rainbow-button/pages/index.tsx
@@ -1,18 +1,27 @@
 import { RainbowButton } from '@rainbow-me/button';
 import type { NextPage } from 'next';
+import { useAccount, useDisconnect } from 'wagmi';
 
 const Home: NextPage = () => {
+  const { isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
   return (
     <div
       style={{
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
+        gap: 10,
       }}
     >
       <RainbowButton />
+      {isConnected && (
+        <button onClick={() => disconnect()}>
+          Disconnect
+        </button>
+      )}
     </div>
   );
 };
