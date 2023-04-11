8a4eb264069bf4c3986bf6b1d1fae14938583036
Daniel Sinclair <d@niel.nyc>
2023-04-11 00:22:17 -0400

    chore: connector cleanup

--- packages/rainbowkit/src/wallets/walletConnectors/rabbyWallet/rabbyWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/rabbyWallet/rabbyWallet.ts
@@ -1,31 +1,33 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
+import type { InjectedConnectorOptions } from '@wagmi/core/connectors/injected';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { Chain } from '../../../components/RainbowKitProvider/RainbowKitChainContext';
 import { Wallet } from '../../Wallet';
 
 export interface RabbyWalletOptions {
   chains: Chain[];
-  shimDisconnect?: boolean;
 }
 
 export const rabbyWallet = ({
   chains,
-  shimDisconnect,
-}: RabbyWalletOptions): Wallet => ({
+  ...options
+}: RabbyWalletOptions & InjectedConnectorOptions): Wallet => ({
   id: 'rabby',
-  name: 'Rabby Wallet',
+  name: 'Rabby',
   iconUrl: async () => (await import('./rabbyWallet.svg')).default,
   iconBackground: '#fff',
-  // @ts-ignore
-  installed: typeof window !== 'undefined' && window.ethereum?.isRabby === true,
+  installed:
+    typeof window !== 'undefined' &&
+    typeof window.ethereum !== 'undefined' &&
+    window.ethereum.isRabby === true,
   downloadUrls: {
     browserExtension:
-      'https://chrome.google.com/webstore/detail/rabby/acmacodkjbdgmoleebolmdjonilkdbch?hl=en',
+      'https://chrome.google.com/webstore/detail/rabby/acmacodkjbdgmoleebolmdjonilkdbch',
   },
   createConnector: () => ({
     connector: new InjectedConnector({
       chains,
-      options: { shimDisconnect },
+      options,
     }),
   }),
 });
