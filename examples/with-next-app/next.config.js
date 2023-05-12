86009c44e8a630f70aba40061dec414e53b54258
Daniel Sinclair <d@niel.nyc>
2023-05-12 17:48:19 -0400

    fix: polyfills for wagmi v1

--- examples/with-next-app/next.config.js
+++ examples/with-next-app/next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
