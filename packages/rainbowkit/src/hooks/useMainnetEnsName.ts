dc1376d420ff686e29c8598586b113ed45d1d97c
Daniel Sinclair <d@niel.nyc>
2023-05-12 13:25:19 -0400

    fix: 0x type

--- packages/rainbowkit/src/hooks/useMainnetEnsName.ts
+++ packages/rainbowkit/src/hooks/useMainnetEnsName.ts
@@ -1,7 +1,7 @@
 import { useEnsName } from 'wagmi';
 import { useMainnet } from './useMainnet';
 
-export function useMainnetEnsName(address: string | undefined) {
+export function useMainnetEnsName(address: `0x${string}` | undefined) {
   const { chainId, enabled } = useMainnet();
 
   const { data: ensName } = useEnsName({
