6572d1817a955131ce39c7b3dc062352a46be467
Daniel Sinclair <d@niel.nyc>
2023-06-02 02:56:43 -0400

    fix: getWalletConnectConnector overloads for strongly typed options for public api, fixed tests

--- packages/rainbowkit/src/wallets/getWalletConnectConnector.ts
+++ packages/rainbowkit/src/wallets/getWalletConnectConnector.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/unified-signatures */
+/* eslint-disable no-redeclare */
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy';
 import { Chain } from '../components/RainbowKitProvider/RainbowKitChainContext';
@@ -29,12 +31,32 @@ function createConnector(
   config: WalletConnectLegacyConnectorConfig | WalletConnectConnectorConfig
 ): WalletConnectLegacyConnector | WalletConnectConnector {
   const connector =
-    version === '2'
-      ? new WalletConnectConnector(config)
-      : new WalletConnectLegacyConnector(config);
+    version === '1'
+      ? new WalletConnectLegacyConnector(config)
+      : new WalletConnectConnector(config);
   sharedConnectors.set(JSON.stringify(config), connector);
   return connector;
 }
+
+export function getWalletConnectConnector(config: {
+  version?: WalletConnectVersion;
+  projectId?: string;
+  chains: Chain[];
+  options?: WalletConnectConnectorOptions;
+}): WalletConnectConnector;
+
+export function getWalletConnectConnector(config: {
+  version: '1';
+  chains: Chain[];
+  options?: WalletConnectLegacyConnectorOptions;
+}): WalletConnectLegacyConnector;
+
+export function getWalletConnectConnector(config: {
+  version: '2';
+  projectId: string;
+  chains: Chain[];
+  options?: WalletConnectConnectorOptions;
+}): WalletConnectConnector;
 
 export function getWalletConnectConnector({
   chains,
@@ -47,14 +69,23 @@ export function getWalletConnectConnector({
   version?: WalletConnectVersion;
   options?: WalletConnectLegacyConnectorOptions | WalletConnectConnectorOptions;
 }): WalletConnectConnector | WalletConnectLegacyConnector {
+  if (version === '2' && !projectId)
+    throw new Error(
+      'No projectId found. Every dApp must now provide a WalletConnect Cloud projectId to enable WalletConnect v2 https://www.rainbowkit.com/docs/installation'
+    );
   const config = {
     chains,
-    options: {
-      projectId,
-      qrcode: false,
-      showQrModal: false,
-      ...options,
-    },
+    options:
+      version === '1'
+        ? {
+            qrcode: false,
+            ...options,
+          }
+        : {
+            projectId,
+            showQrModal: false,
+            ...options,
+          },
   };
 
   const serializedConfig = JSON.stringify(config);
