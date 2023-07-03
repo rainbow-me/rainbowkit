9749ecdf1c1de3756cebf0818a143663197ce0ee
kean.yang <kisenyang@126.com>
2023-07-03 16:49:16 +0800

    feat: add bitkeep wallet connector

---
+++ packages/rainbowkit/src/wallets/index.ts
@@ -0,0 +1,47 @@
+import { argentWallet } from './argentWallet/argentWallet';
+import { bitKeepWallet } from './bitKeepWallet/bitKeepWallet';
+import { bitskiWallet } from './bitskiWallet/bitskiWallet';
+import { braveWallet } from './braveWallet/braveWallet';
+import { coinbaseWallet } from './coinbaseWallet/coinbaseWallet';
+import { dawnWallet } from './dawnWallet/dawnWallet';
+import { imTokenWallet } from './imTokenWallet/imTokenWallet';
+import { injectedWallet } from './injectedWallet/injectedWallet';
+import { ledgerWallet } from './ledgerWallet/ledgerWallet';
+import { metaMaskWallet } from './metaMaskWallet/metaMaskWallet';
+import { mewWallet } from './mewWallet/mewWallet';
+import { okxWallet } from './okxWallet/okxWallet';
+import { omniWallet } from './omniWallet/omniWallet';
+import { phantomWallet } from './phantomWallet/phantomWallet';
+import { rabbyWallet } from './rabbyWallet/rabbyWallet';
+import { rainbowWallet } from './rainbowWallet/rainbowWallet';
+import { safeWallet } from './safeWallet/safeWallet';
+import { tahoWallet } from './tahoWallet/tahoWallet';
+import { trustWallet } from './trustWallet/trustWallet';
+import { walletConnectWallet } from './walletConnectWallet/walletConnectWallet';
+import { xdefiWallet } from './xdefiWallet/xdefiWallet';
+import { zerionWallet } from './zerionWallet/zerionWallet';
+
+export {
+  argentWallet,
+  bitKeepWallet,
+  bitskiWallet,
+  braveWallet,
+  coinbaseWallet,
+  dawnWallet,
+  imTokenWallet,
+  injectedWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  mewWallet,
+  omniWallet,
+  phantomWallet,
+  rabbyWallet,
+  rainbowWallet,
+  safeWallet,
+  tahoWallet,
+  trustWallet,
+  okxWallet,
+  walletConnectWallet,
+  xdefiWallet,
+  zerionWallet,
+};
