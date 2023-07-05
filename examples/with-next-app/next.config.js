457403abcffc4d522564de2c1e7f7f55d9bbb093
Daniel Sinclair <d@niel.nyc>
2023-07-05 13:20:17 -0400

    fix: webpack externals for pino-pretty, lokijs, encoding for next app dir

--- examples/with-next-app/next.config.js
+++ examples/with-next-app/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
