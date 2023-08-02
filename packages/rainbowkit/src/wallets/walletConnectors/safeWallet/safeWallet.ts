af921790493eb16fc6cf442e354649b6a8b5fa4a
Daniel Sinclair <d@niel.nyc>
2023-08-02 15:09:14 -0400

    chore: fix linting

--- packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
@@ -22,7 +22,7 @@ export const safeWallet = ({
     // borrowed from wagmi safe connector
     !(typeof window === 'undefined') && window?.parent !== window,
   downloadUrls: {
-    // We're opting not to provide a download prompt if the application is not 
+    // We're opting not to provide a download prompt if the application is not
     // already running as a Safe App within the context of the Safe browser,
     // since it's unlikely to be a desired behavior for users.
   },
