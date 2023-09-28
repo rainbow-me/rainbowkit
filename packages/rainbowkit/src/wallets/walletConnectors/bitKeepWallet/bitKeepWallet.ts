adba0c62c3445973857b1b1f983a61a73d279b81
kean <kean.yang@bitget.comkean.yang@bitget.com>
2023-09-28 11:03:34 +0800

    feat：run pnpm run format:fix and pnpm run lint:fix

--- packages/rainbowkit/src/wallets/walletConnectors/bitKeepWallet/bitKeepWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/bitKeepWallet/bitKeepWallet.ts
@@ -11,7 +11,6 @@ import type {
   WalletConnectLegacyConnectorOptions,
 } from '../../getWalletConnectConnector';
 
-
 export interface BitgetWalletLegacyOptions {
   projectId?: string;
   chains: Chain[];
