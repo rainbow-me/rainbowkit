1619d5d30882d7a382b6630538a1c85259d3fee0
Daniel Sinclair <d@niel.nyc>
2023-10-03 14:59:58 -0400

    docs: localization

---
+++ site/components/Table/Table.css.ts
@@ -0,0 +1,35 @@
+import { style } from '@vanilla-extract/css';
+import { atoms } from 'css/atoms';
+
+// biome-ignore format: design system keys
+export const regionWrapper = style({
+  'overflow': 'auto',
+  'position': 'relative',
+  ':focus': { outline: 0 },
+});
+
+export const table = style({
+  borderCollapse: 'collapse',
+  textAlign: 'left',
+  width: '100%',
+});
+
+const cell = style([
+  atoms({
+    borderBottomWidth: '1',
+    paddingRight: '5',
+    paddingY: '6',
+  }),
+]);
+
+export const th = style([
+  cell,
+  atoms({ fontSize: '3' }),
+  style({ fontWeight: '800' }),
+]);
+
+export const td = style([
+  cell,
+  atoms({ fontSize: '2' }),
+  style({ whiteSpace: 'nowrap' }),
+]);
