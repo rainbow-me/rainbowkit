424a5cb47d1de1cf8a06ab5bb26e6994c8c597e4
Daniel Sinclair <d@niel.nyc>
2023-10-05 16:45:04 -0400

    refactor: deprecate `standardizeBrowserLocale`

--- packages/rainbowkit/src/components/RainbowKitProvider/I18nContext.tsx
+++ packages/rainbowkit/src/components/RainbowKitProvider/I18nContext.tsx
@@ -1,10 +1,7 @@
 import { I18n as _I18N } from 'i18n-js';
 import React, { ReactNode, createContext, useMemo } from 'react';
 import { Locale, i18n as _i18n } from '../../locales';
-import {
-  getDetectedBrowserLocale,
-  standardizeBrowserLocale,
-} from '../../utils/locale';
+import { detectedBrowserLocale } from '../../utils/locale';
 
 export const I18nContext = createContext<_I18N>(_i18n);
 
@@ -14,24 +11,20 @@ interface I18nProviderProps {
 }
 
 export const I18nProvider = ({ children, locale }: I18nProviderProps) => {
-  const detectedBrowserLocale = useMemo(() => {
-    const browserLocale = getDetectedBrowserLocale();
-
-    if (browserLocale) {
-      const standardizedocale = standardizeBrowserLocale[browserLocale];
-      if (standardizedocale) return standardizedocale;
-    }
-  }, []);
+  const browserLocale: Locale = useMemo(
+    () => detectedBrowserLocale() as Locale,
+    [],
+  );
 
   const i18n = useMemo(() => {
     if (locale) {
       _i18n.locale = locale;
-    } else if (!locale && detectedBrowserLocale) {
-      _i18n.locale = detectedBrowserLocale;
+    } else if (!locale && browserLocale) {
+      _i18n.locale = browserLocale;
     }
 
     return _i18n;
-  }, [locale, detectedBrowserLocale]);
+  }, [locale, browserLocale]);
 
   return <I18nContext.Provider value={i18n}>{children}</I18nContext.Provider>;
 };
--- packages/rainbowkit/src/utils/locale.ts
+++ packages/rainbowkit/src/utils/locale.ts
@@ -1,7 +1,5 @@
-import { Locale } from '../locales';
-
-// Detect user locale from browser
-export const getDetectedBrowserLocale = () => {
+// Detect user locale from browser user agent
+export const detectedBrowserLocale = () => {
   if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
     if (navigator.languages?.length) {
       return navigator.languages[0];
@@ -11,34 +9,4 @@ export const getDetectedBrowserLocale = () => {
       return navigator.language;
     }
   }
-};
-
-// biome-ignore format: locale keys
-export const standardizeBrowserLocale: Record<string | Locale, Locale> = {
-  'ar': 'ar-AR',
-  'ar-AR': 'ar-AR',
-  'en': 'en-US',
-  'en-US': 'en-US',
-  'es': 'es-419',
-  'es-419': 'es-419',
-  'fr': 'fr-FR',
-  'fr-FR': 'fr-FR',
-  'hi': 'hi-IN',
-  'hi-IN': 'hi-IN',
-  'id': 'id-ID',
-  'id-ID': 'id-ID',
-  'ja': 'ja-JP',
-  'ja-JP': 'ja-JP',
-  'ko': 'ko-KR',
-  'ko-KR': 'ko-KR',
-  'pt': 'pt-BR',
-  'pt-BR': 'pt-BR',
-  'ru': 'ru-RU',
-  'ru-RU': 'ru-RU',
-  'th': 'th-TH',
-  'th-TH': 'th-TH',
-  'tr': 'tr-TR',
-  'tr-TR': 'tr-TR',
-  'zh': 'zh-CN',
-  'zh-CN': 'zh-CN',
 };
