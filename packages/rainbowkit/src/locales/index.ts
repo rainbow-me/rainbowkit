d332c97679897e300d56d31fd323ee88124f63fe
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2023-10-06 22:17:28 +0100

    format:fix

--- packages/rainbowkit/src/locales/index.ts
+++ packages/rainbowkit/src/locales/index.ts
@@ -1,5 +1,4 @@
-import type * as I18nTypes from 'i18n-js';
-import { I18n } from 'i18n-js/dist/require/index.js';
+import { I18n } from 'i18n-js';
 import ar_AR from './ar_AR.json';
 import en_US from './en_US.json';
 import es_419 from './es_419.json';
@@ -43,33 +42,33 @@ export type Locale =
   | 'zh-CN';
 
 // biome-ignore format: locale keys
-export const i18n: I18nTypes.I18n = new I18n({
-  ar: ar_AR,
-  "ar-AR": ar_AR,
-  en: en_US,
-  "en-US": en_US,
-  es: es_419,
-  "es-419": es_419,
-  fr: fr_FR,
-  "fr-FR": fr_FR,
-  hi: hi_IN,
-  "hi-IN": hi_IN,
-  id: id_ID,
-  "id-ID": id_ID,
-  ja: ja_JP,
-  "ja-JP": ja_JP,
-  ko: ko_KR,
-  "ko-KR": ko_KR,
-  pt: pt_BR,
-  "pt-BR": pt_BR,
-  ru: ru_RU,
-  "ru-RU": ru_RU,
-  th: th_TH,
-  "th-TH": th_TH,
-  tr: tr_TR,
-  "tr-TR": tr_TR,
-  zh: zh_CN,
-  "zh-CN": zh_CN,
+export const i18n = new I18n({
+  'ar': ar_AR,
+  'ar-AR': ar_AR,
+  'en': en_US,
+  'en-US': en_US,
+  'es': es_419,
+  'es-419': es_419,
+  'fr': fr_FR,
+  'fr-FR': fr_FR,
+  'hi': hi_IN,
+  'hi-IN': hi_IN,
+  'id': id_ID,
+  'id-ID': id_ID,
+  'ja': ja_JP,
+  'ja-JP': ja_JP,
+  'ko': ko_KR,
+  'ko-KR': ko_KR,
+  'pt': pt_BR,
+  'pt-BR': pt_BR,
+  'ru': ru_RU,
+  'ru-RU': ru_RU,
+  'th': th_TH,
+  'th-TH': th_TH,
+  'tr': tr_TR,
+  'tr-TR': tr_TR,
+  'zh': zh_CN,
+  'zh-CN': zh_CN,
 });
 
 i18n.defaultLocale = 'en-US';
