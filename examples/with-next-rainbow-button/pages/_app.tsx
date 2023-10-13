9457f97d9d5e37413617184d07c036397e82c9b3
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2023-10-13 19:08:45 +0100

    fix: bring back wagmi

--- examples/with-next-rainbow-button/pages/_app.tsx
+++ examples/with-next-rainbow-button/pages/_app.tsx
@@ -1,22 +1,33 @@
 import "../styles/global.css";
 import "@rainbow-me/button/styles.css";
 import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-import { RainbowButtonProvider } from "@rainbow-me/button";
+import { RainbowButtonProvider, RainbowConnector } from "@rainbow-me/button";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
 
 const projectId =
   process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "YOUR_PROJECT_ID";
 
+const wagmiClient = createConfig({
+  autoConnect: true,
+  connectors: [new RainbowConnector({ chains, projectId })],
+  publicClient,
+  webSocketPublicClient,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RainbowButtonProvider
-      projectId={projectId}
-      chainProviders={[publicProvider()]}
-      chains={[mainnet]}
-    >
-      <Component {...pageProps} />
-    </RainbowButtonProvider>
+    <WagmiConfig config={wagmiClient}>
+      <RainbowButtonProvider>
+        <Component {...pageProps} />
+      </RainbowButtonProvider>
+    </WagmiConfig>
   );
 }
 
