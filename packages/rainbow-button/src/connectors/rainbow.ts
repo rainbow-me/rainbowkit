01d2bb054a543eab9ff774423ceb93764a1d6218
Daniel Sinclair <d@niel.nyc>
2023-10-31 17:00:20 -0400

    chore: remove eslint-disable overrides

--- packages/rainbow-button/src/connectors/rainbow.ts
+++ packages/rainbow-button/src/connectors/rainbow.ts
@@ -1,4 +1,3 @@
-/*eslint-disable no-redeclare */
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import type { RainbowWalletOptions } from '@rainbow-me/rainbowkit/dist/wallets/walletConnectors/rainbowWallet/rainbowWallet';
 import { rainbowWallet } from '@rainbow-me/rainbowkit/wallets';
