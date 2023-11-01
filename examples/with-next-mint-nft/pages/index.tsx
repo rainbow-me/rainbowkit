fe17081fdb2273d15606ae51adf4ba3e374ed3a4
Daniel Sinclair <d@niel.nyc>
2023-11-01 15:21:02 -0400

    fix: legacy image error in with-next-mint-nft

--- examples/with-next-mint-nft/pages/index.tsx
+++ examples/with-next-mint-nft/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import {
