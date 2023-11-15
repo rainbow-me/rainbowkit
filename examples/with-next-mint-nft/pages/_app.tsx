6f2d88dc355ac1148d8f87a902274894b17336d1
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2023-11-15 14:10:38 +0000

    fix: prettify

--- examples/with-next-mint-nft/pages/_app.tsx
+++ examples/with-next-mint-nft/pages/_app.tsx
@@ -13,24 +13,24 @@ import { Chain, defineChain } from 'viem';
 
 const rinkeby: Chain = defineChain({
   id: 4,
-  name: "Rinkeby",
-  nativeCurrency: { name: "Rinkeby Ether", symbol: "ETH", decimals: 18 },
+  name: 'Rinkeby',
+  nativeCurrency: { name: 'Rinkeby Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
-    alchemy: { http: ["https://eth-rinkeby.alchemyapi.io/v2"] },
-    default: { http: ["https://rpc.ankr.com/eth_rinkeby"] },
-    infura: { http: ["https://rinkeby.infura.io/v3"] },
-    public: { http: ["https://rpc.ankr.com/eth_rinkeby"] },
+    alchemy: { http: ['https://eth-rinkeby.alchemyapi.io/v2'] },
+    default: { http: ['https://rpc.ankr.com/eth_rinkeby'] },
+    infura: { http: ['https://rinkeby.infura.io/v3'] },
+    public: { http: ['https://rpc.ankr.com/eth_rinkeby'] },
   },
   blockExplorers: {
-    etherscan: { name: "Etherscan", url: "https://rinkeby.etherscan.io" },
-    default: { name: "Etherscan", url: "https://rinkeby.etherscan.io" },
+    etherscan: { name: 'Etherscan', url: 'https://rinkeby.etherscan.io' },
+    default: { name: 'Etherscan', url: 'https://rinkeby.etherscan.io' },
   },
   contracts: {
     ensRegistry: {
-      address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
     },
     multicall3: {
-      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 10299530,
     },
   },
