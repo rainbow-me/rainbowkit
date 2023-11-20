b35e37800b120c21f7ff3366be861e6e19bf899f
Daniel Sinclair <d@niel.nyc>
2023-11-20 01:34:38 -0500

    chore: remove unclear comment

--- packages/rainbowkit/src/components/ConnectOptions/ConnectOptions.tsx
+++ packages/rainbowkit/src/components/ConnectOptions/ConnectOptions.tsx
@@ -8,9 +8,6 @@ import { MobileOptions } from './MobileOptions';
 export default function ConnectOptions({ onClose }: { onClose: () => void }) {
   const { connector } = useContext(RainbowButtonContext);
 
-  // If you do inspect element on mobile then switch to
-  // desktop view the rainbowkit connect modal will pop up.
-  // In this case we don't want that if we are using a custom connector
   return isMobile() ? (
     connector ? (
       <MobileLinking onClose={onClose} />
