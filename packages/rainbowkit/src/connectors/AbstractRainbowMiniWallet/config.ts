2c9fd0c744c51236c42a2491118edc8f8e962f43
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2023-12-14 13:17:15 +0000

    fix: replace it with mini wallets

--- packages/rainbowkit/src/connectors/AbstractRainbowMiniWallet/config.ts
+++ packages/rainbowkit/src/connectors/AbstractRainbowMiniWallet/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   target: 'auth',
-  url: 'https://rainbowkit-example-git-mago-mini-wallets-rainbowdotme.vercel.app',
+  url: 'https://mini-wallet-rainbowdotme.vercel.app',
   features: 'width=600,height=700',
 };
