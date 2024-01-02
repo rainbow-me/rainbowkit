048b11539abeeae42a86c0c432c4ce5363ce0242
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2024-01-02 21:47:14 +0000

    chore: remove index.ts

--- packages/rainbowkit/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { ConnectButton } from './ConnectButton/ConnectButton';
-export { WalletButton } from './WalletButton/WalletButton';
-export { RainbowKitProvider } from './RainbowKitProvider/RainbowKitProvider';
