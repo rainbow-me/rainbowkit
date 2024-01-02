eb2a189b6460c4c0bcdb3e565e7e3b1201bb1cf6
Daniel Sinclair <d@niel.nyc>
2024-01-02 17:03:49 -0500

    Revert "chore: remove index.ts"
    This reverts commit 048b11539abeeae42a86c0c432c4ce5363ce0242.

---
+++ packages/rainbowkit/src/components/index.ts
@@ -0,0 +1,3 @@
+export { ConnectButton } from './ConnectButton/ConnectButton';
+export { WalletButton } from './WalletButton/WalletButton';
+export { RainbowKitProvider } from './RainbowKitProvider/RainbowKitProvider';
