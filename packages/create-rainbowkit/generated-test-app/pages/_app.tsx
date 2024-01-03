e893ff70c24bf91215cc71b6a264d978ca7f0ade
Daniel Sinclair <d@niel.nyc>
2024-01-03 02:02:03 -0500

    revert: force react import

--- packages/create-rainbowkit/generated-test-app/pages/_app.tsx
+++ packages/create-rainbowkit/generated-test-app/pages/_app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
