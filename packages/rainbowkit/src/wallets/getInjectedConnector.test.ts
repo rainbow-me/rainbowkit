db1eaec106bd622536ae067c0d7c1181a53b489a
Daniel Sinclair <d@niel.nyc>
2024-01-07 01:06:51 -0500

    feat: namespace support in getInjectedConnector

--- packages/rainbowkit/src/wallets/getInjectedConnector.test.ts
+++ packages/rainbowkit/src/wallets/getInjectedConnector.test.ts
@@ -21,7 +21,7 @@ describe('getInjectedConnector', () => {
   it('only metamask provider', () => {
     window.ethereum = { isMetaMask: true } as WindowProvider;
     const connector = getInjectedConnector({
-      flag: 'isRainbow',
+      flag: 'isMetaMask',
       chains,
     });
     expect(connector.name).toEqual('MetaMask');
@@ -79,13 +79,13 @@ describe('getInjectedConnector', () => {
 describe('hasInjectedProvider', () => {
   it('only rainbow provider', () => {
     window.ethereum = { isMetaMask: true, isRainbow: true } as WindowProvider;
-    const hasRainbow = hasInjectedProvider('isRainbow');
+    const hasRainbow = hasInjectedProvider({ flag: 'isRainbow' });
     expect(hasRainbow).toEqual(true);
   });
 
   it('only metamask provider', () => {
     window.ethereum = { isMetaMask: true } as WindowProvider;
-    const hasRainbow = hasInjectedProvider('isRainbow');
+    const hasRainbow = hasInjectedProvider({ flag: 'isRainbow' });
     expect(hasRainbow).toEqual(false);
   });
 
@@ -94,7 +94,7 @@ describe('hasInjectedProvider', () => {
       isMetaMask: true,
       isCoinbaseWallet: true,
     } as WindowProvider;
-    const hasCoinbase = hasInjectedProvider('isCoinbaseWallet');
+    const hasCoinbase = hasInjectedProvider({ flag: 'isCoinbaseWallet' });
     expect(hasCoinbase).toEqual(true);
   });
 
@@ -108,10 +108,10 @@ describe('hasInjectedProvider', () => {
       ],
     } as WindowProvider;
 
-    const hasCoinbase = hasInjectedProvider('isCoinbaseWallet');
+    const hasCoinbase = hasInjectedProvider({ flag: 'isCoinbaseWallet' });
     expect(hasCoinbase).toEqual(true);
 
-    const hasRainbow = hasInjectedProvider('isRainbow');
+    const hasRainbow = hasInjectedProvider({ flag: 'isRainbow' });
     expect(hasRainbow).toEqual(true);
   });
 
@@ -126,13 +126,13 @@ describe('hasInjectedProvider', () => {
       ],
     } as WindowProvider;
 
-    const hasCoinbase = hasInjectedProvider('isCoinbaseWallet');
+    const hasCoinbase = hasInjectedProvider({ flag: 'isCoinbaseWallet' });
     expect(hasCoinbase).toEqual(true);
 
-    const hasRainbow = hasInjectedProvider('isRainbow');
+    const hasRainbow = hasInjectedProvider({ flag: 'isRainbow' });
     expect(hasRainbow).toEqual(true);
 
-    const hasMetaMask = hasInjectedProvider('isMetaMask');
+    const hasMetaMask = hasInjectedProvider({ flag: 'isMetaMask' });
     expect(hasMetaMask).toEqual(true);
   });
 });
--- packages/rainbowkit/src/wallets/getInjectedConnector.ts
+++ packages/rainbowkit/src/wallets/getInjectedConnector.ts
@@ -6,32 +6,61 @@ import type { Chain } from '../components/RainbowKitProvider/RainbowKitChainCont
  * Returns the explicit window provider that matches the flag and the flag is true
  */
 function getExplicitInjectedProvider(
-  flag: keyof InjectedProviderFlags,
+  flag: keyof InjectedProviderFlags | string,
 ): WindowProvider | undefined {
   if (typeof window === 'undefined' || typeof window.ethereum === 'undefined')
     return;
   const providers = window.ethereum.providers;
   return providers
-    ? providers.find((provider) => provider[flag])
-    : window.ethereum[flag]
+    ? // @ts-expect-error - some provider flags are not typed in `InjectedProviderFlags`
+      providers.find((provider) => provider[flag])
+    : // @ts-expect-error - some provider flags are not typed in `InjectedProviderFlags`
+      window.ethereum[flag]
       ? window.ethereum
       : undefined;
 }
 
-export function hasInjectedProvider(
-  flag: keyof InjectedProviderFlags,
-): boolean {
-  return Boolean(getExplicitInjectedProvider(flag));
+/*
+ * Gets the `window.namespace` window provider if it exists
+ */
+function getWindowProviderNamespace(
+  namespace: string,
+): WindowProvider | undefined {
+  // @ts-expect-error - only `window.ethereum` is available in wagmi's global namespace
+  return window[namespace];
+}
+
+/*
+ * Checks if the explict provider or window ethereum exists
+ */
+export function hasInjectedProvider({
+  flag,
+  namespace,
+}: {
+  flag?: keyof InjectedProviderFlags | string;
+  namespace?: string;
+}): boolean {
+  if (namespace && typeof getWindowProviderNamespace(namespace) !== 'undefined')
+    return true;
+  if (flag && typeof getExplicitInjectedProvider(flag) !== 'undefined')
+    return true;
+  return false;
 }
 
 /*
  * Returns an injected provider that favors the flag match, but falls back to window.ethereum
  */
 function getInjectedProvider(
-  flag: keyof InjectedProviderFlags,
+  flag: keyof InjectedProviderFlags | string,
+  namespace?: string,
 ): WindowProvider | undefined {
   if (typeof window === 'undefined' || typeof window.ethereum === 'undefined')
     return;
+  if (namespace) {
+    // prefer custom eip1193 namespaces
+    const windowProvider = getWindowProviderNamespace(namespace);
+    if (windowProvider) return windowProvider;
+  }
   const providers = window.ethereum.providers;
   const provider = getExplicitInjectedProvider(flag);
   if (provider) return provider;
@@ -43,14 +72,16 @@ function getInjectedProvider(
 export function getInjectedConnector({
   chains,
   flag,
+  namespace,
 }: {
-  flag?: keyof InjectedProviderFlags;
+  flag?: keyof InjectedProviderFlags | string;
+  namespace?: string;
   chains: Chain[];
 }): InjectedConnector {
   return new InjectedConnector({
     chains,
     options: flag
-      ? { getProvider: () => getInjectedProvider(flag) }
+      ? { getProvider: () => getInjectedProvider(flag, namespace) }
       : undefined,
   });
 }
