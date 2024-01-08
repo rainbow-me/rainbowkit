fe759d1f6405d2f69733fbbb630f109e007b0c10
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2024-01-08 18:36:21 +0000

    fix: format document

--- packages/create-rainbowkit/generated-test-app/pages/_app.tsx
+++ packages/create-rainbowkit/generated-test-app/pages/_app.tsx
@@ -1,9 +1,9 @@
-import "../styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider, http } from "wagmi";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider, http } from 'wagmi';
 import {
   arbitrum,
   base,
@@ -13,8 +13,8 @@ import {
   sepolia,
   zora,
   Chain,
-} from "wagmi/chains";
-import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+} from 'wagmi/chains';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 
 const chains: readonly [Chain, ...Chain[]] = [
   mainnet,
@@ -23,12 +23,12 @@ const chains: readonly [Chain, ...Chain[]] = [
   arbitrum,
   base,
   zora,
-  ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
 ];
 
 const config = getDefaultConfig({
-  appName: "RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
   chains,
   transports: {
     [mainnet.id]: http(),
