6a609e0b5b14a030099a2c51ebb827547de056f9
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2024-01-15 23:34:31 +0000

    fix: handle unhandled rejection silent

--- packages/rainbowkit/test/setup.ts
+++ packages/rainbowkit/test/setup.ts
@@ -2,6 +2,10 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
+process.on('unhandledRejection', (reason) => {
+  console.log(`FAILED TO HANDLE PROMISE REJECTION: ${JSON.stringify(reason)}`);
+});
+
 afterEach(() => {
   cleanup();
 });
