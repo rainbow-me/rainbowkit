75f7aab9cd28fe61278d06f1894a05d02e119be2
Daniel Sinclair <d@niel.nyc>
2024-01-16 12:07:40 -0500

    Revert "fix: handle unhandled rejection silent"
    This reverts commit 6a609e0b5b14a030099a2c51ebb827547de056f9.

--- packages/rainbowkit/test/setup.ts
+++ packages/rainbowkit/test/setup.ts
@@ -2,10 +2,6 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
-process.on('unhandledRejection', (reason) => {
-  console.log(`FAILED TO HANDLE PROMISE REJECTION: ${JSON.stringify(reason)}`);
-});
-
 afterEach(() => {
   cleanup();
 });
