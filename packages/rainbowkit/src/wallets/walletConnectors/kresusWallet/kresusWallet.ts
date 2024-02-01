6f439e84696e888f344832219bb989a247c45f24
Chanchal Delson <chanchal.d@netobjex.com>
2024-02-01 20:13:16 +0530

    feat: kresusWallet

--- packages/rainbowkit/src/wallets/walletConnectors/kresusWallet/kresusWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/kresusWallet/kresusWallet.ts
@@ -18,10 +18,9 @@ export const kresusWallet = ({
   iconBackground: '#0c2f78',
   downloadUrls: {
     android:
-      'hhttps://play.google.com/store/apps/details?id=com.kresus.superapp',
+      'https://play.google.com/store/apps/details?id=com.kresus.superapp',
     ios: 'https://apps.apple.com/us/app/kresus-crypto-nft-superapp/id6444355152',
-    chrome: '',
-    qrCode: 'https://kresus.com/',
+    qrCode: 'https://kresusconnect.kresus.com/download',
   },
   createConnector: () => {
     const connector = getWalletConnectConnector({ projectId, chains });
@@ -43,47 +42,6 @@ export const kresusWallet = ({
             provider.once('display_uri', resolve),
           );
           return uri;
-        },
-        instructions: {
-          learnMoreUrl: 'https://my-wallet/learn-more',
-          steps: [
-            {
-              description: '',
-              step: 'install',
-              title: 'Open the My Wallet app',
-            },
-            {
-              description:
-                'After you scan, a connection prompt will appear for you to connect your wallet.',
-              step: 'scan',
-              title: 'Tap the scan button',
-            },
-          ],
-        },
-      },
-      extension: {
-        instructions: {
-          learnMoreUrl: 'https://kresus.com',
-          steps: [
-            {
-              description:
-                'We recommend pinning My Wallet to your taskbar for quicker access to your wallet.',
-              step: 'install',
-              title: 'Install the My Wallet extension',
-            },
-            {
-              description:
-                'Be sure to back up your wallet using a secure method. Never share your secret phrase with anyone.',
-              step: 'create',
-              title: 'Create or Import a Wallet',
-            },
-            {
-              description:
-                'Once you set up your wallet, click below to refresh the browser and load up the extension.',
-              step: 'refresh',
-              title: 'Refresh your browser',
-            },
-          ],
         },
       },
     };
