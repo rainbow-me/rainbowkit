5a21e153880a4d11a4979c905207f84333ee6eee
Chanchal Delson <chanchal.d@netobjex.com>
2024-02-01 16:55:32 +0530

    feat: kresusWallet

--- packages/rainbowkit/src/wallets/walletConnectors/kresusWallet/kresusWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/kresusWallet/kresusWallet.ts
@@ -17,10 +17,11 @@ export const kresusWallet = ({
     'https://media.licdn.com/dms/image/C4D0BAQFvPaHJbKWuhg/company-logo_200_200/0/1669189963337?e=2147483647&v=beta&t=1hfdXtEztnGcow9Le8z6B2_XEXYqB0pJQXsvPbYNH-U',
   iconBackground: '#0c2f78',
   downloadUrls: {
-    android: 'https://play.google.com/store/apps/details?id=my.wallet',
-    ios: 'https://apps.apple.com/us/app/my-wallet',
-    chrome: 'https://chrome.google.com/webstore/detail/my-wallet',
-    qrCode: 'https://my-wallet/qr',
+    android:
+      'hhttps://play.google.com/store/apps/details?id=com.kresus.superapp',
+    ios: 'https://apps.apple.com/us/app/kresus-crypto-nft-superapp/id6444355152',
+    chrome: '',
+    qrCode: 'https://kresus.com/',
   },
   createConnector: () => {
     const connector = getWalletConnectConnector({ projectId, chains });
