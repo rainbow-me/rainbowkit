f178c6fc081802a7f7a473922813eb8a052cfefa
Daniel Sinclair <d@niel.nyc>
2024-02-26 00:55:53 -0500

    Revert "chore: deprecate webpack externals"
    This reverts commit d24dd61a6db8a9dc791ceb47fc6942ab3f394e6e.

--- examples/with-next-app-i18n/next.config.js
+++ examples/with-next-app-i18n/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
