8b17b8fbec55ce252660ee89b7c129fb62f3a1f3
Daniel Sinclair <d@niel.nyc>
2024-02-26 14:13:50 -0500

    chore: adjust example webpack configs (#1803)
    * chore: deprecate walletconnect v1
    
    * chore: deprecate webpack externals
    
    * Revert "chore: deprecate webpack externals"
    
    This reverts commit d24dd61a6db8a9dc791ceb47fc6942ab3f394e6e.

--- examples/with-next-app/next.config.js
+++ examples/with-next-app/next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
