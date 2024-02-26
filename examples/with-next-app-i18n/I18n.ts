e88ebfc50f7df7a099fc6101c93b2892e5197e9a
Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
2024-02-26 18:09:57 +0000

    chore: tweak I18n.ts

--- examples/with-next-app-i18n/I18n.ts
+++ examples/with-next-app-i18n/I18n.ts
@@ -1,11 +1,6 @@
-import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-const locales = ['en-US', 'zh-CN'];
-
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as any)) notFound();
-
+export default getRequestConfig(() => {
   return {
     messages: {}, // We don't use any messages
   };
