d24dd61a6db8a9dc791ceb47fc6942ab3f394e6e
Daniel Sinclair <d@niel.nyc>
2024-02-26 00:19:26 -0500

    chore: deprecate webpack externals

--- examples/with-next-app-i18n/next.config.js
+++ examples/with-next-app-i18n/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
-  },
 };
 
 module.exports = nextConfig;
