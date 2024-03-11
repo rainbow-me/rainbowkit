430b89a5f63a5beb317204f9356f3f336edffe86
Daniel Sinclair <d@niel.nyc>
2024-03-11 05:13:22 -0400

    chore: lint fix

--- packages/rainbowkit/src/components/ConnectOptions/DesktopOptions.tsx
+++ packages/rainbowkit/src/components/ConnectOptions/DesktopOptions.tsx
@@ -77,9 +77,9 @@ export function DesktopOptions({ onClose }: { onClose: () => void }) {
   const { connector } = useContext(WalletButtonContext);
 
   // The `WalletButton` component made the connect modal appear empty when trying to connect.
-  // This happened because of a mix up between EIP-6963 and RainbowKit connectors. 
-  // The problem was finding the correct `wallet.id`. `WalletButton` uses RainbowKit's id, 
-  // but EIP-6963 uses `rdns` for its id. We now don't merge EIP-6963 and RainbowKit 
+  // This happened because of a mix up between EIP-6963 and RainbowKit connectors.
+  // The problem was finding the correct `wallet.id`. `WalletButton` uses RainbowKit's id,
+  // but EIP-6963 uses `rdns` for its id. We now don't merge EIP-6963 and RainbowKit
   // connectors if user interacts with `WalletButton` component.
   const mergeEIP6963WithRkConnectors = !connector;
 
