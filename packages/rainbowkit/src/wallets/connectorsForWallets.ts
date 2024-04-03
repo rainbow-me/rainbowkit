4dd1e45539a018b1bcc0ab5089f27618fca082ec
MK <53529533+magiziz@users.noreply.github.com>
2024-04-03 20:59:26 +0100

    fix: de-dupe wallets in connectorsForWallets (#1910)
    * chore: de-dupe wallets
    
    * chore: small tweak
    
    * chore: format
    
    * Update sour-bobcats-dance.md
    
    ---------
    
    Co-authored-by: Magomed Khamidov <53529533+KosmosKey@users.noreply.github.com>
    Co-authored-by: Daniel Sinclair <d@niel.nyc>

--- packages/rainbowkit/src/wallets/connectorsForWallets.ts
+++ packages/rainbowkit/src/wallets/connectorsForWallets.ts
@@ -1,6 +1,7 @@
 import type { CreateConnectorFn } from 'wagmi';
 import { isHexString } from '../utils/colors';
 import { omitUndefinedValues } from '../utils/omitUndefinedValues';
+import { uniqueBy } from '../utils/uniqueBy';
 import type {
   RainbowKitWalletConnectParameters,
   Wallet,
@@ -93,12 +94,13 @@ export const connectorsForWallets = (
     });
   });
 
+  // Filtering out duplicated wallets in case there is any.
   // We process the known visible wallets first so that the potentially
   // hidden wallets have access to the complete list of resolved wallets
-  const walletListItems: WalletListItem[] = [
-    ...visibleWallets,
-    ...potentiallyHiddenWallets,
-  ];
+  const walletListItems: WalletListItem[] = uniqueBy(
+    [...visibleWallets, ...potentiallyHiddenWallets],
+    'id',
+  );
 
   for (const {
     createConnector,
