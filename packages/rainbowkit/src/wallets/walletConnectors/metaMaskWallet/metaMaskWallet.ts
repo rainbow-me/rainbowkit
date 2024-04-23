c3e8de8367f044707719940bc118daa9378503c3
Daniel Sinclair <d@niel.nyc>
2024-04-23 01:12:32 -0400

    fix: provider failover if providers array is empty

--- packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
@@ -122,11 +122,8 @@ export const metaMaskWallet = ({
             chains,
             options: {
               getProvider: () =>
-                providers
-                  ? providers.find(isMetaMask)
-                  : typeof window !== 'undefined'
-                    ? window.ethereum
-                    : undefined,
+                providers?.find(isMetaMask) ||
+                (typeof window !== 'undefined' ? window.ethereum : undefined),
               ...options,
             },
           });
