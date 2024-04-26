515498f17c26cac5b8626f58b1d6ff88b8eaabc2
MK <53529533+magiziz@users.noreply.github.com>
2024-04-26 08:10:46 +0100

    chore: replace wagmi `coinbaseWallet` with custom connector (#1945)
    * chore: coinbase connector
    
    * chore: changeset
    
    * chore: tweak
    
    * chore: force headless mode
    
    * chore: headless mode true
    
    * chore: add comment for connector source
    
    * revert: headlessMode change
    
    * chore: amend changeset
    
    * chore: revert to @coinbase/wallet-sdk v3.6.6
    
    * chore: upgrade @coinbase/wallet-sdk to v3.7.2
    
    * chore: trial @coinbase/wallet-sdk v3.9.3
    
    ---------
    
    Co-authored-by: Daniel Sinclair <d@niel.nyc>

--- packages/rainbowkit/src/wallets/walletConnectors/coinbaseWallet/coinbaseWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/coinbaseWallet/coinbaseWallet.ts
@@ -1,5 +1,5 @@
 import { createConnector } from 'wagmi';
-import { coinbaseWallet as coinbaseWagmiWallet } from 'wagmi/connectors';
+import { coinbaseWallet as coinbaseWagmiWallet } from '../../../connectors/coinbaseWallet';
 import { isIOS } from '../../../utils/isMobile';
 import { Wallet, WalletDetailsParams } from '../../Wallet';
 import { hasInjectedProvider } from '../../getInjectedConnector';
