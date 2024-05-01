888888d5936da46e4fe6403da833e1e7ff20fbd0
Eric Kuhn <erickuhn19@gmail.com>
2024-05-01 16:59:38 -0400

    add kraken wallet

---
+++ packages/rainbowkit/src/wallets/walletConnectors/krakenWallet/krakenWallet.ts
@@ -0,0 +1,54 @@
+import { Wallet } from '../../Wallet';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
+import { DefaultWalletOptions } from './../../Wallet';
+
+export type KrakenWalletOptions = DefaultWalletOptions;
+
+export const krakenWallet = ({
+  projectId,
+  walletConnectParameters,
+}: KrakenWalletOptions): Wallet => ({
+  id: 'kraken',
+  name: 'Kraken Wallet',
+  iconUrl: async () => (await import('./krakenWallet.svg')).default,
+  iconBackground: '#FFD8EA',
+  downloadUrls: {
+    ios: 'https://apps.apple.com/us/app/kraken-wallet/id1626327149',
+    mobile: 'https://kraken.com/wallet',
+    qrCode: 'https://kraken.com/wallet',
+  },
+
+  mobile: {
+    getUri: (uri: string) => {
+      return `krakenwallet://wc?uri=${encodeURIComponent(uri)}`;
+    },
+  },
+  qrCode: {
+    getUri: (uri: string) => uri,
+    instructions: {
+      learnMoreUrl: 'https://kraken.com/wallet',
+      steps: [
+        {
+          description: 'wallet_connectors.kraken.qr_code.step1.description',
+          step: 'install',
+          title: 'wallet_connectors.kraken.qr_code.step1.title',
+        },
+        {
+          description: 'wallet_connectors.kraken.qr_code.step2.description',
+          step: 'create',
+          title: 'wallet_connectors.kraken.qr_code.step2.title',
+        },
+        {
+          description: 'wallet_connectors.kraken.qr_code.step3.description',
+          step: 'scan',
+          title: 'wallet_connectors.kraken.qr_code.step3.title',
+        },
+      ],
+    },
+  },
+
+  createConnector: getWalletConnectConnector({
+    projectId,
+    walletConnectParameters,
+  }),
+});
