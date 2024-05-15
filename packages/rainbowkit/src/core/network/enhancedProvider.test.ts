972766b3c780fdd26d3e114db8236b329f0dcb33
MK <mago.khamidov@gmail.com>
2024-05-15 22:23:59 +0100

    chore: describeIf instead of it itif

--- packages/rainbowkit/src/core/network/enhancedProvider.test.ts
+++ packages/rainbowkit/src/core/network/enhancedProvider.test.ts
@@ -1,20 +1,22 @@
 import { describe, expect, it } from 'vitest';
 import { enhancedProviderHttp } from './enhancedProvider';
 
-const itif = () =>
-  typeof process.env.RAINBOW_PROVIDER_API_KEY !== 'undefined' ? it : it.skip;
+const describeIf = () =>
+  typeof process.env.RAINBOW_PROVIDER_API_KEY === 'undefined'
+    ? describe.skip
+    : describe;
 
-describe('createHttpClient', () => {
-  itif()("should return 'ok' status for health check endpoint", async () => {
+describeIf()('createHttpClient', () => {
+  it("should return 'ok' status for health check endpoint", async () => {
     const { data } = await enhancedProviderHttp.get('/healthcheck');
     expect(data).toStrictEqual({ status: 'ok' });
   });
 
-  itif()('should throw an error if operation is aborted', async () => {
+  it('should throw an error if operation is aborted', async () => {
     await expect(() => enhancedProviderHttp.get('/unknown')).rejects.toThrow();
   });
 
-  itif()("should throw an error if endpoint doesn't exist", async () => {
+  it("should throw an error if endpoint doesn't exist", async () => {
     await expect(() => enhancedProviderHttp.get('/unknown')).rejects.toThrow(
       'Not Found',
     );
