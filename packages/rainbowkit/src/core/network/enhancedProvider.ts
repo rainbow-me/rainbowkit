888c3438ef8f24363f6333faea97ce4704fa102d
Daniel Sinclair <d@niel.nyc>
2024-05-21 19:02:47 -0400

    fix: check if process is defined if vite polyfill not available

--- packages/rainbowkit/src/core/network/enhancedProvider.ts
+++ packages/rainbowkit/src/core/network/enhancedProvider.ts
@@ -3,6 +3,10 @@ import { createHttpClient } from './internal/createHttpClient';
 export const enhancedProviderHttp = createHttpClient({
   baseUrl: 'https://enhanced-provider.rainbow.me',
   headers: {
-    'x-api-key': '__rainbowProviderApiKey',
+    'x-api-key':
+      (typeof process !== 'undefined' &&
+        typeof process.env !== 'undefined' &&
+        process.env.RAINBOW_PROVIDER_API_KEY) ||
+      '__rainbowProviderApiKey',
   },
 });
