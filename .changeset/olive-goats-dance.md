c8aa81c9f61921fe7701434d2ad04570468cbbb1
MK <mago.khamidov@gmail.com>
2024-05-21 16:17:47 +0100

    fix: process.env crashing in vite

---
+++ .changeset/olive-goats-dance.md
@@ -0,0 +1,5 @@
+---
+"@rainbow-me/rainbowkit": patch
+---
+
+Fixed a bug where `process.env.RAINBOW_PROVIDER_API_KEY` was crashing when running vite in dev mode.
--- packages/rainbowkit/src/core/network/enhancedProvider.ts
+++ packages/rainbowkit/src/core/network/enhancedProvider.ts
@@ -3,7 +3,6 @@ import { createHttpClient } from './internal/createHttpClient';
 export const enhancedProviderHttp = createHttpClient({
   baseUrl: 'https://enhanced-provider.rainbow.me',
   headers: {
-    'x-api-key':
-      process.env.RAINBOW_PROVIDER_API_KEY ?? '__rainbowProviderApiKey',
+    'x-api-key': '__rainbowProviderApiKey',
   },
 });
