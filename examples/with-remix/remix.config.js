66f307890e172c46983637d80e9bc2abd004515c
Daniel Sinclair <d@niel.nyc>
2024-05-22 01:08:36 -0400

    revert: remove browserNodeBuiltinsPolyfill from remix

--- examples/with-remix/remix.config.js
+++ examples/with-remix/remix.config.js
@@ -5,4 +5,7 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  browserNodeBuiltinsPolyfill: {
+    modules: { buffer: true, events: true, http: true },
+  },
 };
