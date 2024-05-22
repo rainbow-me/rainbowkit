a2b62b9390d7549300f39bb50efe442231573912
Daniel Sinclair <d@niel.nyc>
2024-05-22 00:53:31 -0400

    chore: remove web-vitals

--- examples/with-create-react-app/src/index.tsx
+++ examples/with-create-react-app/src/index.tsx
@@ -2,7 +2,6 @@ import '@rainbow-me/rainbowkit/styles.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
@@ -47,8 +46,3 @@ root.render(
     </WagmiProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
