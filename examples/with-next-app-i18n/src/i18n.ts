e079078fc23550a029bf8a406f1a5496c5bc9f51
Daniel Sinclair <d@niel.nyc>
2024-05-29 12:52:50 -0400

    chore: migrate with-next-app-i18n middleware to src/

--- examples/with-next-app-i18n/src/i18n.ts
+++ examples/with-next-app-i18n/src/i18n.ts
@@ -8,6 +8,6 @@ export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale)) notFound();
 
   return {
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
