2dda603e955ff3839ef06386fb0b0d57e88a00b4
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2024-08-30 22:11:41 -0400

    fix: bloom desktop-only behavior (#2169)

--- packages/rainbowkit/src/wallets/walletConnectors/bloomWallet/bloomWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/bloomWallet/bloomWallet.ts
@@ -1,4 +1,3 @@
-import { isMobile } from '../../../utils/isMobile';
 import { DefaultWalletOptions, Wallet } from '../../Wallet';
 import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 
@@ -6,42 +5,13 @@ export const bloomWallet = ({
   projectId,
   walletConnectParameters,
 }: DefaultWalletOptions): Wallet => ({
-  id: 'bloomWallet',
+  id: 'bloom',
   name: 'Bloom Wallet',
   iconBackground: '#000',
   iconAccent: '#000',
-  hidden: () => isMobile(),
   iconUrl: async () => (await import('./bloomWallet.svg')).default,
   downloadUrls: {
-    qrCode: 'https://bloomwallet.io/',
-    windows: 'https://bloomwallet.io/',
-    macos: 'https://bloomwallet.io/',
-    linux: 'https://bloomwallet.io/',
     desktop: 'https://bloomwallet.io/',
-  },
-  qrCode: {
-    getUri: (uri: string) =>
-      `bloom://wallet-connect/wc?uri=${encodeURIComponent(uri)}`,
-    instructions: {
-      learnMoreUrl: 'https://bloomwallet.io/',
-      steps: [
-        {
-          description: 'wallet_connectors.bloom.qr_code.step1.description',
-          step: 'install',
-          title: 'wallet_connectors.bloom.qr_code.step1.title',
-        },
-        {
-          description: 'wallet_connectors.bloom.qr_code.step2.description',
-          step: 'create',
-          title: 'wallet_connectors.bloom.qr_code.step2.title',
-        },
-        {
-          description: 'wallet_connectors.bloom.qr_code.step3.description',
-          step: 'refresh',
-          title: 'wallet_connectors.bloom.qr_code.step3.title',
-        },
-      ],
-    },
   },
   desktop: {
     getUri: (uri: string) =>
