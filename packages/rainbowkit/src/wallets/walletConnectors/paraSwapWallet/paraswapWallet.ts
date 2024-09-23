6ffa9c32e597ce826bb7c48512f0776532a8587e
proinsights <proinsights@gmail.com>
2024-09-23 17:00:38 +0100

    chore: prettify

--- packages/rainbowkit/src/wallets/walletConnectors/paraSwapWallet/paraswapWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/paraSwapWallet/paraswapWallet.ts
@@ -1,6 +1,6 @@
-import type { Wallet } from "../../Wallet";
-import { getWalletConnectConnector } from "../../getWalletConnectConnector";
-import type { DefaultWalletOptions } from "../../Wallet";
+import type { Wallet } from '../../Wallet';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
+import type { DefaultWalletOptions } from '../../Wallet';
 
 export type ParaSwapWalletOptions = DefaultWalletOptions;
 
@@ -8,14 +8,14 @@ export const paraSwapWallet = ({
   projectId,
   walletConnectParameters,
 }: ParaSwapWalletOptions): Wallet => ({
-  id: "paraswap",
-  name: "ParaSwap Wallet",
-  iconUrl: async () => (await import("./paraSwapWallet.svg")).default,
-  iconBackground: "#578CFC",
+  id: 'paraswap',
+  name: 'ParaSwap Wallet',
+  iconUrl: async () => (await import('./paraSwapWallet.svg')).default,
+  iconBackground: '#578CFC',
   downloadUrls: {
-    ios: "https://apps.apple.com/us/app/paraswap-multichain-wallet/id1584610690",
-    mobile: "https://paraswap.io",
-    qrCode: "https://paraswap.io",
+    ios: 'https://apps.apple.com/us/app/paraswap-multichain-wallet/id1584610690',
+    mobile: 'https://paraswap.io',
+    qrCode: 'https://paraswap.io',
   },
   mobile: {
     getUri: (uri: string) => {
@@ -25,22 +25,22 @@ export const paraSwapWallet = ({
   qrCode: {
     getUri: (uri: string) => uri,
     instructions: {
-      learnMoreUrl: "https://paraswap.io",
+      learnMoreUrl: 'https://paraswap.io',
       steps: [
         {
-          description: "wallet_connectors.paraswap.qr_code.step1.description",
-          step: "install",
-          title: "wallet_connectors.paraswap.qr_code.step1.title",
+          description: 'wallet_connectors.paraswap.qr_code.step1.description',
+          step: 'install',
+          title: 'wallet_connectors.paraswap.qr_code.step1.title',
         },
         {
-          description: "wallet_connectors.paraswap.qr_code.step2.description",
-          step: "create",
-          title: "wallet_connectors.paraswap.qr_code.step2.title",
+          description: 'wallet_connectors.paraswap.qr_code.step2.description',
+          step: 'create',
+          title: 'wallet_connectors.paraswap.qr_code.step2.title',
         },
         {
-          description: "wallet_connectors.paraswap.qr_code.step3.description",
-          step: "scan",
-          title: "wallet_connectors.paraswap.qr_code.step3.title",
+          description: 'wallet_connectors.paraswap.qr_code.step3.description',
+          step: 'scan',
+          title: 'wallet_connectors.paraswap.qr_code.step3.title',
         },
       ],
     },
