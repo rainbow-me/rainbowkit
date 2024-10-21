4e5261058b5987bc1b57505bea926c9bcee5fa5a
Sergiy Pash <gitserg@pm.me>
2024-10-21 22:42:20 +0100

    feat: wigwam wallet support (#2206)
    * Wigwam wallet support
    
    * feat: wigwam wallet support
    
    * fix: rollback additional translations
    
    * fix: rollback single quote
    
    * fix: format docs

---
+++ packages/rainbowkit/src/wallets/walletConnectors/wigwamWallet/wigwamWallet.ts
@@ -0,0 +1,49 @@
+import type { Wallet } from '../../Wallet';
+import {
+  getInjectedConnector,
+  hasInjectedProvider,
+} from '../../getInjectedConnector';
+
+export const wigwamWallet = (): Wallet => {
+  return {
+    id: 'wigwam',
+    name: 'Wigwam',
+    iconBackground: '#80EF6E',
+    iconUrl: async () => (await import('./wigwamWallet.svg')).default,
+    downloadUrls: {
+      chrome:
+        'https://chromewebstore.google.com/detail/wigwam-%E2%80%94-web3-wallet/lccbohhgfkdikahanoclbdmaolidjdfl',
+      browserExtension: 'https://wigwam.app',
+    },
+    installed: hasInjectedProvider({
+      namespace: 'wigwamEthereum',
+      flag: 'isWigwam',
+    }),
+    extension: {
+      instructions: {
+        learnMoreUrl: 'https://wigwam.app/',
+        steps: [
+          {
+            description: 'wallet_connectors.wigwam.extension.step1.description',
+            step: 'install',
+            title: 'wallet_connectors.wigwam.extension.step1.title',
+          },
+          {
+            description: 'wallet_connectors.wigwam.extension.step2.description',
+            step: 'create',
+            title: 'wallet_connectors.wigwam.extension.step2.title',
+          },
+          {
+            description: 'wallet_connectors.wigwam.extension.step3.description',
+            step: 'refresh',
+            title: 'wallet_connectors.wigwam.extension.step3.title',
+          },
+        ],
+      },
+    },
+    createConnector: getInjectedConnector({
+      namespace: 'wigwamEthereum',
+      flag: 'isWigwam',
+    }),
+  };
+};
