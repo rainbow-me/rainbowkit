5eb6a4f0647d7d06bf3d89838488a09cd3e4c390
Edouard Bougon <15703023+EdouardBougon@users.noreply.github.com>
2024-10-22 18:52:53 +0200

    feat: improve vite example

--- examples/with-vite/src/wagmi.ts
+++ examples/with-vite/src/wagmi.ts
@@ -1,9 +1,44 @@
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { 
+  metaMaskWallet, 
+  safeWallet, 
+  rainbowWallet, 
+  coinbaseWallet, 
+  walletConnectWallet,
+  rabbyWallet,
+  trustWallet,
+  uniswapWallet
+} from '@rainbow-me/rainbowkit/wallets';
+
+// Enable Smart Wallet and EOA
+// Testing `preference` type
+coinbaseWallet.preference = 'all';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit demo',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  projectId: import.meta.env.VITE_PUBLIC_WALLETCONNECT_PROJECT_ID ?? 'YOUR_PROJECT_ID',
+  chains: [mainnet, 
+    polygon, optimism, arbitrum, base],
+  wallets: [
+    {
+      groupName: 'Popular',
+      wallets: [
+        safeWallet,
+        rainbowWallet,
+        coinbaseWallet,
+        metaMaskWallet,
+        walletConnectWallet,
+      ],
+    },
+    {
+      groupName: 'Other',
+      wallets: [
+        rabbyWallet,
+        trustWallet,
+        uniswapWallet,
+      ],
+    },
+  ]
 });
