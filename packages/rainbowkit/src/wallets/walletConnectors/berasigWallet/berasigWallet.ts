9dd23d9e350c430622e15a629bab78d7cc89c566
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2024-12-02 14:54:23 -0500

    feat: support berasigWallet (#2253)
    * fix: lint
    
    * chore: changeset
    
    * feat: berasigWallet support
    
    feat: berasigWallet support
    
    fix: format
    
    revert locale files
    
    feat: support BerasigWallet
    
    fix: example dapp support
    
    chore: docs
    
    ---------
    
    Co-authored-by: thiennv-qng <81727176+thiennv-qng@users.noreply.github.com>

---
+++ packages/rainbowkit/src/wallets/walletConnectors/berasigWallet/berasigWallet.ts
@@ -0,0 +1,67 @@
+import type { DefaultWalletOptions, Wallet } from '../../Wallet';
+import {
+  getInjectedConnector,
+  hasInjectedProvider,
+} from '../../getInjectedConnector';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
+
+export type BerasigWalletOptions = DefaultWalletOptions;
+
+export const berasigWallet = ({
+  projectId,
+  walletConnectParameters,
+}: BerasigWalletOptions): Wallet => {
+  const isBerasigWalletInjected = hasInjectedProvider({
+    namespace: 'berasig.ethereum',
+  });
+
+  const shouldUseWalletConnect = !isBerasigWalletInjected;
+  return {
+    id: 'berasig',
+    name: 'BeraSig',
+    iconUrl: async () => (await import('./berasigWallet.svg')).default,
+    iconBackground: '#ffffff',
+    installed: isBerasigWalletInjected,
+    downloadUrls: {
+      android: 'https://play.google.com/store/apps/details?id=io.berasig.ios',
+      ios: 'https://apps.apple.com/us/app/berasig-wallet-on-berachain/id6502052535',
+      qrCode: 'https://berasig.com',
+      mobile: 'https://berasig.com',
+      browserExtension:
+        'https://chromewebstore.google.com/detail/berasig/ckedkkegjbflcfblcjklibnedmfjppbj',
+    },
+    extension: {
+      instructions: {
+        steps: [
+          {
+            description:
+              'wallet_connectors.berasig.extension.step1.description',
+            step: 'install',
+            title: 'wallet_connectors.berasig.extension.step1.title',
+          },
+          {
+            description:
+              'wallet_connectors.berasig.extension.step2.description',
+            step: 'create',
+            title: 'wallet_connectors.berasig.extension.step2.title',
+          },
+          {
+            description:
+              'wallet_connectors.berasig.extension.step3.description',
+            step: 'refresh',
+            title: 'wallet_connectors.berasig.extension.step3.title',
+          },
+        ],
+        learnMoreUrl: 'https://berasig.com',
+      },
+    },
+    createConnector: shouldUseWalletConnect
+      ? getWalletConnectConnector({
+          projectId,
+          walletConnectParameters,
+        })
+      : getInjectedConnector({
+          namespace: 'berasig.ethereum',
+        }),
+  };
+};
