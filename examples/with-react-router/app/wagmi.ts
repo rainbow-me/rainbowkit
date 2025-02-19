49017bbb9aeb32f6740713a684af38542300af6b
jgresham <johnsgresham@gmail.com>
2025-02-19 13:34:57 -0800

    exmaple: react-router with required changes

---
+++ examples/with-react-router/app/wagmi.ts
@@ -0,0 +1,16 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true,
+});
