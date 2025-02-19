d34ee702bfe85540b3a3588ad207e431dec3af72
Daniel Sinclair <d@niel.nyc>
2025-02-19 01:08:07 -0500

    fix: ignore generated-test-app in changesets

--- packages/create-rainbowkit/generated-test-app/next.config.js
+++ packages/create-rainbowkit/generated-test-app/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   },
 };
 
