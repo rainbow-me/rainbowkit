182440a9a9bda4bf5f721425472a18f434a63e4f
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2025-02-19 02:26:16 -0500

    chore: wagmi, viem, pnpm upgrade (#2316)
    * chore: upgrade wagmi, viem, pnpm
    
    chore: lockfile
    
    Revert "fix: remove @vercel/speed-insights"
    
    This reverts commit f47376bdf73af6bd7126a3ba2274360c66eef999.
    
    fix: remove @vercel/speed-insights
    
    * chore: changeset
    
    * fix: pnpmfile matching checksum
    
    * fix: pnpmfileChecksum consistency
    
    * fix: ignore generated-test-app in changesets
    
    * fix: eslint warning
    
    * fix: generated-test-app added to build step
    
    * chore: pinned ignoredBuiltDependencies
    
    chore: ignore build scripts
    
    fix: ignore onlyBuiltDependencies
    
    * Revert "fix: pnpmfileChecksum consistency"
    
    This reverts commit b8e084af568dcbb2f1f2478befd3a5849f5974f3.
    
    * fix: rebuild generated-test-app on husky when templates change
    
    * revert: re-enable lavamoat preinstall-always-fail
    
    * Revert "revert: re-enable lavamoat preinstall-always-fail"
    
    This reverts commit a38a6405b46532db781d3247cee27060abe69bd9.

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,7 +2,7 @@ function readPackage(pkg) {
   // Filter dependencies in app templates that are present in the root package.json.
   // This allows us to provide complete package.json files for all app templates.
   if (
-    /-app$/.test(pkg.name) || // create-rainbowkit templates (e.g. next-app)
+    /^packages\/create-rainbowkit\/.*-app$/.test(pkg.name) || // create-rainbowkit templates (e.g. next-app)
     /^with-/.test(pkg.name) // example apps (e.g. with-next)
   ) {
     pkg.dependencies = omitRootDependencies(pkg.name, pkg.dependencies);
