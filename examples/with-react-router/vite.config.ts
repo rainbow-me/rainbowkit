f266e96eb85436e254ac4f689c207d7ef115b41a
jgresham <johnsgresham@gmail.com>
2025-02-19 13:34:57 -0800

    exmaple: react-router with required changes

--- examples/with-react-router/vite.config.ts
+++ examples/with-react-router/vite.config.ts
@@ -1,8 +1,22 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    nodePolyfills({
+      include: ['buffer', 'events', 'http'],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+  // server: {
+  //   allowedHosts: true, // allow any host (ex. ngrok for testing on mobile)
+  // }
 });
