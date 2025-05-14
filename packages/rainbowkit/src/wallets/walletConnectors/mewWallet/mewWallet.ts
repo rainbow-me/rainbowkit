5b546492a8cd041ed96785c808bf442a3ce870f0
Gamaliel 'Yel' Padillo <gamalielhere@users.noreply.github.com>
2025-05-13 19:00:19 -0700

    feat: update mewWallet integration to use walletConnect (#2385)
    * feat: update mewWallet integration to use walletConnect
    
    * revert: pnpm checksum
    
    * fix: injected vs walletconnect handling
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: Daniel Sinclair <d@niel.nyc>

--- packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/mewWallet/mewWallet.ts
@@ -1,16 +1,30 @@
-import type { Wallet } from '../../Wallet';
+import type { DefaultWalletOptions, Wallet } from '../../Wallet';
 import {
   getInjectedConnector,
   hasInjectedProvider,
 } from '../../getInjectedConnector';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 
-export const mewWallet = (): Wallet => {
+export type MEWWalletOptions = DefaultWalletOptions;
+
+export const mewWallet = ({
+  projectId,
+  walletConnectParameters,
+}: MEWWalletOptions): Wallet => {
+  const isMEWInjected = hasInjectedProvider({ flag: 'isMEWwallet' });
+  const shouldUseWalletConnect = !isMEWInjected;
+
+  const getUri = (uri: string) => {
+    // MEW Wallet uses associated domain instead of url scheme
+    return `https://mewwallet.com/wc?uri=${encodeURIComponent(uri)}`;
+  };
+
   return {
     id: 'mew',
     name: 'MEW wallet',
     iconUrl: async () => (await import('./mewWallet.svg')).default,
     iconBackground: '#fff',
-    installed: hasInjectedProvider({ flag: 'isMEWwallet' }),
+    installed: !shouldUseWalletConnect ? isMEWInjected : undefined,
     downloadUrls: {
       android:
         'https://play.google.com/store/apps/details?id=com.myetherwallet.mewwallet&referrer=utm_source%3Drainbow',
@@ -18,6 +32,38 @@ export const mewWallet = (): Wallet => {
       mobile: 'https://mewwallet.com',
       qrCode: 'https://mewwallet.com',
     },
-    createConnector: getInjectedConnector({ flag: 'isMEWwallet' }),
+    mobile: { getUri: shouldUseWalletConnect ? getUri : undefined },
+    qrCode: shouldUseWalletConnect
+      ? {
+          getUri,
+          instructions: {
+            learnMoreUrl:
+              'https://help.myetherwallet.com/en/articles/5946588-create-and-back-up-your-wallet-with-mew-wallet-ios',
+            steps: [
+              {
+                description: 'wallet_connectors.mew.qr_code.step1.description',
+                step: 'install',
+                title: 'wallet_connectors.mew.qr_code.step1.title',
+              },
+              {
+                description: 'wallet_connectors.mew.qr_code.step2.description',
+                step: 'create',
+                title: 'wallet_connectors.mew.qr_code.step2.title',
+              },
+              {
+                description: 'wallet_connectors.mew.qr_code.step3.description',
+                step: 'scan',
+                title: 'wallet_connectors.mew.qr_code.step3.title',
+              },
+            ],
+          },
+        }
+      : undefined,
+    createConnector: shouldUseWalletConnect
+      ? getWalletConnectConnector({
+          projectId,
+          walletConnectParameters,
+        })
+      : getInjectedConnector({ flag: 'isMEWwallet' }),
   };
 };
