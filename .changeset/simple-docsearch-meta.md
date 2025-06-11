a8edb090c20cc66500b4dfcd8c78f7054256518d
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2025-06-11 15:20:18 -0400

    feat(site): add docsearch meta (#2438)
    * fix(site): simplify docsearch version
    
    * fix(site): check vercel env for docsearch

---
+++ .changeset/simple-docsearch-meta.md
@@ -0,0 +1,5 @@
+---
+"site": patch
+---
+
+Add docsearch meta tags with npm tag logic based on major version.
--- site/pages/docs/[slug].tsx
+++ site/pages/docs/[slug].tsx
@@ -3,8 +3,20 @@ import { components } from 'components/MdxComponents/MdxComponents';
 import { TitleAndMetaTags } from 'components/TitleAndMetaTags/TitleAndMetaTags';
 import { docsRoutes } from 'lib/docsRoutes';
 import { useLiveReload, useMDXComponent } from 'next-contentlayer/hooks';
+import Head from 'next/head';
 import React from 'react';
+import pckg from '../../../packages/rainbowkit/package.json';
 import { type Doc, allDocs } from '.contentlayer/generated';
+
+const RAINBOWKIT_VERSION = pckg.version as string;
+
+const isProd =
+  process.env.NODE_ENV === 'production' &&
+  process.env.VERCEL_ENV === 'production';
+
+const DOCSEARCH_VERSION = isProd
+  ? `${RAINBOWKIT_VERSION},latest`
+  : RAINBOWKIT_VERSION;
 
 type DocPageProps = { doc: Doc; sectionName: string };
 
@@ -18,6 +30,10 @@ export default function DocPage({ doc, sectionName }: DocPageProps) {
         description={doc.description}
         title={`${doc.title} — RainbowKit`}
       />
+      <Head>
+        <meta name="docsearch:language" content={doc.locale} />
+        <meta name="docsearch:version" content={DOCSEARCH_VERSION} />
+      </Head>
       <Box as="article">
         <p data-algolia-lvl0 style={{ display: 'none' }}>
           {sectionName}
