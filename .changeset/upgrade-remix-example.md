829115a41c3d34fb9690c982b86383ba54d8970f
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2025-06-11 23:26:47 -0400

    chore: update remix example

--- .changeset/upgrade-remix-example.md
+++ .changeset/upgrade-remix-example.md
@@ -2,4 +2,5 @@
 examples/with-remix: patch
 ---
 
-Upgrade Remix dependencies to 2.16.8.
+Upgrade Remix dependencies to 2.16.8 and enable React Router v7 future flags in
+`remix.config.js`.
--- examples/with-remix/remix.config.js
+++ examples/with-remix/remix.config.js
@@ -1,6 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
