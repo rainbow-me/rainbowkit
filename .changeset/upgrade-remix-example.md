2c092b63e84896a78a4406a629c070c4ab0d897c
Daniel Sinclair <d@niel.nyc>
2025-06-12 00:59:24 -0400

    Revert "chore: update remix example"
    This reverts commit 829115a41c3d34fb9690c982b86383ba54d8970f.

--- .changeset/upgrade-remix-example.md
+++ .changeset/upgrade-remix-example.md
@@ -2,5 +2,4 @@
 examples/with-remix: patch
 ---
 
-Upgrade Remix dependencies to 2.16.8 and enable React Router v7 future flags in
-`remix.config.js`.
+Upgrade Remix dependencies to 2.16.8.
--- examples/with-remix/remix.config.js
+++ examples/with-remix/remix.config.js
@@ -1,13 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
-  future: {
-    v3_fetcherPersist: true,
-    v3_lazyRouteDiscovery: true,
-    v3_relativeSplatPath: true,
-    v3_singleFetch: true,
-    v3_throwAbortReason: true,
-  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
