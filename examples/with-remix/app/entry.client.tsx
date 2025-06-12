17441ea23aa8d50eef0a6c4ce88aacb2ae8e829c
Daniel Sinclair <d@niel.nyc>
2025-06-12 00:56:39 -0400

    Revert "fix: use streaming entry for remix example"
    This reverts commit 13157a1f14c9540b270e52a46217361c29e41e13.

--- examples/with-remix/app/entry.client.tsx
+++ examples/with-remix/app/entry.client.tsx
@@ -1,12 +1,4 @@
 import { RemixBrowser } from '@remix-run/react';
 import { hydrateRoot } from 'react-dom/client';
-import { startTransition, StrictMode } from 'react';
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>,
-  );
-});
+hydrateRoot(document, <RemixBrowser />);
