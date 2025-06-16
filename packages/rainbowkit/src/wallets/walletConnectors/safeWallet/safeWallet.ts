b773bfd5e34bd197cff9a3d9d01568892afdaf30
Daniel Sinclair <d@niel.nyc>
2025-06-16 18:19:45 -0400

    feat: walletconnect support for safe mobile

--- packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/safeWallet/safeWallet.ts
@@ -1,31 +1,51 @@
 import { createConnector } from 'wagmi';
 import { safe } from 'wagmi/connectors';
-import type { Wallet, WalletDetailsParams } from '../../Wallet';
+import type {
+  DefaultWalletOptions,
+  Wallet,
+  WalletDetailsParams,
+} from '../../Wallet';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 
-export const safeWallet = (): Wallet => ({
-  id: 'safe',
-  name: 'Safe',
-  iconAccent: '#12ff80',
-  iconBackground: '#fff',
-  iconUrl: async () => (await import('./safeWallet.svg')).default,
-  installed:
-    // Only allowed in iframe context
-    // borrowed from wagmi safe connector
-    !(typeof window === 'undefined') && window?.parent !== window,
-  downloadUrls: {
-    android:
-      'https://play.google.com/store/apps/details?id=io.gnosis.safe&hl=en_US',
-    ios: 'https://apps.apple.com/us/app/safe-wallet/id1515759131',
-    desktop: 'https://app.safe.global/',
-  },
-  mobile: {
-    getUri: (uri: string) =>
-      `https://app.safe.global/wc?uri=${encodeURIComponent(uri)}`,
-  },
-  createConnector: (walletDetails: WalletDetailsParams) => {
-    return createConnector((config) => ({
-      ...safe()(config),
-      ...walletDetails,
-    }));
-  },
-});
+export const safeWallet = ({
+  projectId,
+  walletConnectParameters,
+}: DefaultWalletOptions): Wallet => {
+  // Only allowed in iframe context
+  // borrowed from wagmi safe connector
+  const isIframe =
+    !(typeof window === 'undefined') && window?.parent !== window;
+  const shouldUseWalletConnect = !isIframe;
+
+  return {
+    id: 'safe',
+    name: 'Safe',
+    iconAccent: '#12ff80',
+    iconBackground: '#fff',
+    iconUrl: async () => (await import('./safeWallet.svg')).default,
+    installed: !shouldUseWalletConnect ? isIframe : undefined,
+    downloadUrls: {
+      android:
+        'https://play.google.com/store/apps/details?id=io.gnosis.safe&hl=en_US',
+      ios: 'https://apps.apple.com/us/app/safe-wallet/id1515759131',
+      desktop: 'https://app.safe.global',
+    },
+    mobile: {
+      getUri: (uri: string) => uri,
+    },
+    qrCode: {
+      getUri: (uri: string) => uri,
+    },
+    createConnector: shouldUseWalletConnect
+      ? getWalletConnectConnector({
+          projectId,
+          walletConnectParameters,
+        })
+      : (walletDetails: WalletDetailsParams) => {
+          return createConnector((config) => ({
+            ...safe()(config),
+            ...walletDetails,
+          }));
+        },
+  };
+};
