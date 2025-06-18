ff896e3da415c56d93929bd3037b7493d52f3b94
Daniel Sinclair <d@niel.nyc>
2025-06-17 23:29:19 -0400

    fix: prefer walletconnect for desktop qr code

--- packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
@@ -6,18 +6,20 @@ import type {
   WalletDetailsParams,
 } from '../../Wallet';
 import { hasInjectedProvider } from '../../getInjectedConnector';
+import { getWalletConnectConnector } from '../../getWalletConnectConnector';
+import { isMobile } from '../../../utils/isMobile';
 
 export type MetaMaskWalletOptions = DefaultWalletOptions;
 
 export const metaMaskWallet = ({
-  projectId: _projectId,
+  projectId,
   walletConnectParameters,
 }: MetaMaskWalletOptions): Wallet => {
   const isMetaMaskInjected = hasInjectedProvider({ flag: 'isMetaMask' });
-  const shouldUseQRCode = !isMetaMaskInjected;
 
-  // MetaMask mobile deep linking handled by wagmi, return URI unchanged.
-  const getUri = (uri: string) => uri;
+  // TODO: This is a temporary solution to prefer WalletConnect for desktop qr code.
+  const shouldUseWalletConnect = !isMetaMaskInjected && !isMobile();
+  const shouldUseMetaMaskConnector = isMetaMaskInjected || isMobile();
 
   return {
     id: 'metaMask',
@@ -26,7 +28,7 @@ export const metaMaskWallet = ({
     iconUrl: async () => (await import('./metaMaskWallet.svg')).default,
     iconAccent: '#f6851a',
     iconBackground: '#fff',
-    installed: !shouldUseQRCode ? isMetaMaskInjected : undefined,
+    installed: isMetaMaskInjected ? isMetaMaskInjected : undefined,
     downloadUrls: {
       android: 'https://play.google.com/store/apps/details?id=io.metamask',
       ios: 'https://apps.apple.com/us/app/metamask/id1438144202',
@@ -40,11 +42,13 @@ export const metaMaskWallet = ({
       browserExtension: 'https://metamask.io/download',
     },
     mobile: {
-      getUri: shouldUseQRCode ? getUri : undefined,
+      // MetaMask mobile deep linking handled by wagmi, return URI unchanged.
+      getUri: shouldUseMetaMaskConnector ? (uri: string) => uri : undefined,
     },
-    qrCode: shouldUseQRCode
+    qrCode: shouldUseWalletConnect
       ? {
-          getUri,
+          getUri: (uri: string) =>
+            `https://metamask.app.link/wc?uri=${encodeURIComponent(uri)}`,
           instructions: {
             learnMoreUrl: 'https://metamask.io/faqs/',
             steps: [
@@ -95,36 +99,42 @@ export const metaMaskWallet = ({
         ],
       },
     },
-    createConnector: (walletDetails: WalletDetailsParams) => {
-      return createConnector((config) => {
-        const metamaskConnector = metaMask({
-          dappMetadata: {
-            connector: 'rainbowKit',
-            name: walletConnectParameters?.metadata?.name,
-            iconUrl: walletConnectParameters?.metadata?.icons[0],
-            url: walletConnectParameters?.metadata?.url,
-          },
-          headless: true,
-        })(config);
+    createConnector: shouldUseWalletConnect
+      ? getWalletConnectConnector({
+          projectId,
+          walletConnectParameters,
+        })
+      : // MetaMask connector
+        (walletDetails: WalletDetailsParams) => {
+          return createConnector((config) => {
+            const metamaskConnector = metaMask({
+              dappMetadata: {
+                connector: 'rainbowkit',
+                name: walletConnectParameters?.metadata?.name,
+                iconUrl: walletConnectParameters?.metadata?.icons[0],
+                url: walletConnectParameters?.metadata?.url,
+              },
+              headless: true,
+            })(config);
 
-        /**
-         * Override getChainId to avoid metamask error
-         *
-         * @see https://github.com/rainbow-me/rainbowkit/blob/cdcaa25d66b522119852502f71c8efc02b1abdd9/packages/rainbowkit/src/wallets/useWalletConnectors.ts#L57
-         * And @see https://github.com/wevm/wagmi/blob/275cccb51437908a2d7d3dab0549c6050b6340d3/packages/connectors/src/metaMask.ts#L154
-         */
-        return {
-          ...metamaskConnector,
-          ...walletDetails,
-          getChainId: async () => {
-            try {
-              return await metamaskConnector.getChainId();
-            } catch {
-              return config.chains[0]?.id ?? 1;
-            }
-          },
-        };
-      });
-    },
+            /**
+             * Override getChainId to avoid metamask error
+             *
+             * @see https://github.com/rainbow-me/rainbowkit/blob/cdcaa25d66b522119852502f71c8efc02b1abdd9/packages/rainbowkit/src/wallets/useWalletConnectors.ts#L57
+             * And @see https://github.com/wevm/wagmi/blob/275cccb51437908a2d7d3dab0549c6050b6340d3/packages/connectors/src/metaMask.ts#L154
+             */
+            return {
+              ...metamaskConnector,
+              ...walletDetails,
+              getChainId: async () => {
+                try {
+                  return await metamaskConnector.getChainId();
+                } catch {
+                  return config.chains[0]?.id ?? 1;
+                }
+              },
+            };
+          });
+        },
   };
 };
