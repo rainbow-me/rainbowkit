f1948bb87c55327ea3773d146608f38d83ab23bf
Daniel Sinclair <d@niel.nyc>
2025-06-18 00:32:53 -0400

    fix: explicit metamask provider check

--- packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/metaMaskWallet/metaMaskWallet.ts
@@ -8,14 +8,68 @@ import type {
 import { hasInjectedProvider } from '../../getInjectedConnector';
 import { getWalletConnectConnector } from '../../getWalletConnectConnector';
 import { isMobile } from '../../../utils/isMobile';
+import type { WindowProvider } from '../../../types/utils';
 
 export type MetaMaskWalletOptions = DefaultWalletOptions;
+
+function isMetaMask(ethereum?: WindowProvider['ethereum']): boolean {
+  // Logic borrowed from wagmi's legacy MetaMaskConnector
+  // Non-exhaustive list of wallets that try to make themselves look like MetaMask
+  if (!ethereum?.isMetaMask) return false;
+  // Brave tries to make itself look like MetaMask
+  // Could also try RPC `web3_clientVersion` if following is unreliable
+  if (ethereum.isBraveWallet && !ethereum._events && !ethereum._state)
+    return false;
+  if (ethereum.isApexWallet) return false;
+  if (ethereum.isAvalanche) return false;
+  if (ethereum.isBackpack) return false;
+  if (ethereum.isBifrost) return false;
+  if (ethereum.isBitKeep) return false;
+  if (ethereum.isBitski) return false;
+  if (ethereum.isBlockWallet) return false;
+  if (ethereum.isCoinbaseWallet) return false;
+  if (ethereum.isDawn) return false;
+  if (ethereum.isEnkrypt) return false;
+  if (ethereum.isExodus) return false;
+  if (ethereum.isFrame) return false;
+  if (ethereum.isFrontier) return false;
+  if (ethereum.isGamestop) return false;
+  if (ethereum.isHyperPay) return false;
+  if (ethereum.isImToken) return false;
+  if (ethereum.isKuCoinWallet) return false;
+  if (ethereum.isMathWallet) return false;
+  if (ethereum.isNestWallet) return false;
+  if (ethereum.isOkxWallet || ethereum.isOKExWallet) return false;
+  if (ethereum.isOneInchIOSWallet || ethereum.isOneInchAndroidWallet)
+    return false;
+  if (ethereum.isOpera) return false;
+  if (ethereum.isPhantom) return false;
+  if (ethereum.isPortal) return false;
+  if (ethereum.isRabby) return false;
+  if (ethereum.isRainbow) return false;
+  if (ethereum.isStatus) return false;
+  if (ethereum.isTalisman) return false;
+  if (ethereum.isTally) return false;
+  if (ethereum.isTokenPocket) return false;
+  if (ethereum.isTokenary) return false;
+  if (ethereum.isTrust || ethereum.isTrustWallet) return false;
+  if (ethereum.isXDEFI) return false;
+  if (ethereum.isZeal) return false;
+  if (ethereum.isZerion) return false;
+  if (ethereum.__seif) return false;
+  return true;
+}
 
 export const metaMaskWallet = ({
   projectId,
   walletConnectParameters,
 }: MetaMaskWalletOptions): Wallet => {
-  const isMetaMaskInjected = hasInjectedProvider({ flag: 'isMetaMask' });
+  // Custom logic to explicitly detect MetaMask
+  // Whereas hasInjectedProvider only checks for impersonated `isMetaMask`
+  // We need this because MetaMask SDK hangs on impersonated wallets
+  // Previously MetaMask provider would trigger for impersonated wallets
+  const isMetaMaskInjected =
+    typeof window !== 'undefined' ? isMetaMask(window.ethereum) : false;
 
   // TODO: This is a temporary solution to prefer WalletConnect for desktop qr code.
   const shouldUseWalletConnect = !isMetaMaskInjected && !isMobile();
