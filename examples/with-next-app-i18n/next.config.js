268eed723e9ee29e1f2950361632cd8451fc3b8a
Daniel Sinclair <d@niel.nyc>
2025-12-21 20:33:43 -0500

    chore: createNextIntlPlugin

--- examples/with-next-app-i18n/next.config.js
+++ examples/with-next-app-i18n/next.config.js
@@ -1,6 +1,7 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 
-const withNextIntl = createNextIntlPlugin();
+// Explicitly specify the i18n config path for Turbopack compatibility
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
