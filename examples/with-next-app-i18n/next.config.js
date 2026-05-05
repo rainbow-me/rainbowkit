32cef6525bbd62ae0f9abecdf7001d2b4572351e
Daniel Sinclair <d@niel.nyc>
2026-05-05 13:50:20 -0400

    example: polyfill connector browser builds

--- examples/with-next-app-i18n/next.config.js
+++ examples/with-next-app-i18n/next.config.js
@@ -6,6 +6,14 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'pino-pretty': false,
+    };
+
+    return config;
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
